c27b51c6c6467a735a2c1318d572d7350ec50366
ravishende <ravi.sameer.shende@gmail.com>
2023-07-17 12:39:36 -0700

    added template for handling many queries

--- main.py
+++ main.py
@@ -3,16 +3,16 @@ import requests
 from bs4 import BeautifulSoup
 from utils import *
 
-data = query_api_site()
-print(data.text)
-write_json(data)
+# data = query_api_site()
+# print(data.text)
+# write_json(data)
 
-# url = "https://grafana.nrp-nautilus.io/d/85a562078cdf77779eaa1add43ccec1e/kubernetes-compute-resources-namespace-pods?orgId=1&var-datasource=thanos&var-cluster=&var-namespace=wifire&from=1756419957757&to=1806913309834"
-# all_data = requests.get(url)
+url = "https://grafana.nrp-nautilus.io/d/85a562078cdf77779eaa1add43ccec1e/kubernetes-compute-resources-namespace-pods?orgId=1&var-datasource=thanos&var-cluster=&var-namespace=wifire&from=1756419957757&to=1806913309834"
+all_data = requests.get(url)
 
-# s = BeautifulSoup(all_data.content, "html.parser")
-# print(s)
-# print("successfully requested url")
+s = BeautifulSoup(all_data.content, "html.parser")
+print(s)
+print("successfully requested url")
 
 # print(s.text)
 # res = s.find(id="containername")
--- utils.py
+++ utils.py
@@ -1,9 +1,33 @@
 import requests
 import json
 
+def query_select():
+	queries = [
+		"rate(node_cpu_seconds_total[2m])",	#Per-second rate of increase, averaged over last 2 minutes
+		# "irate(node_cpu_seconds_total[1m])", #Per-second rate of increase, calculated over last two samples in a 1 minute window
+		# "increase(node_cpu_seconds_total[5h])", #Absolute increase over last 5 hours
+		
+		#"", #CPU Utilisation (from requests) 
+		#"", #Memory Utilisation (from requests)
+		#"" #Memory Utilisation (from limits)
+		
+	]
+	return queries
+
+def check_queries():
+	base_url = 'https://thanos.nrp-nautilus.io/api/v1/'
+	queries_list = query_select()
+	for i in range(0,len(queries_list)-1):
+		endpoint = f'query?query={queries_list[i]}'
+		full_url = base_url + endpoint
+		data = requests.get(full_url)
+
+		with open(f'queries_test_{i}.json',"w") as file:
+			json.dump(data.json(),file)
+
+
 def query_api_site():
 	base_url = 'https://thanos.nrp-nautilus.io/api/v1/'
-	query = 'rate(container_cpu_usage_seconds_total{namespace="wifire-quicfire"}[3h])'
 	query = 'rate(node_cpu_seconds_total[1m])'
 	endpoint = f'query?query={query}'
 	full_url = base_url + endpoint
