85cc6fc39f5a6041aee7a339a7d41fe1109167d8
Devin Shende <devin.s.shende@gmail.com>
2023-07-17 09:33:00 -0700

    renamed file

--- main.py
+++ main.py
@@ -1,26 +1,9 @@
 import json
 import requests
 from bs4 import BeautifulSoup
+from utils import *
 
-def query_api_site():
-	base_url = 'https://thanos.nrp-nautilus.io/api/v1/'
-	query = 'rate(container_cpu_usage_seconds_total{namespace="wifire-quicfire"}[3h])'
-	endpoint = f'query?query={query}'
-	full_url = base_url + endpoint
-	cpu_data = requests.get(full_url)
-	return cpu_data
-
-def write_json(data):
-	with open("webscrape.json","w") as file:
-		json.dump(data.json(),file)
-
-def read_json():
-	with open("webscrape.json","r") as file:
-		data = json.load(file)
-	return data
-
-data = query_api_site()
-write_json(data)
+data = read_json()
 
 
 # url = "https://grafana.nrp-nautilus.io/d/85a562078cdf77779eaa1add43ccec1e/kubernetes-compute-resources-namespace-pods?orgId=1&var-datasource=thanos&var-cluster=&var-namespace=wifire&from=1756419957757&to=1806913309834"
