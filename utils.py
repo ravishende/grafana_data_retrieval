67b5993fc9a764cb5ca689658ae9dfed8c028f97
Devin Shende <devin.s.shende@gmail.com>
2023-07-17 12:56:17 -0700

    new queries

--- utils.py
+++ utils.py
@@ -3,15 +3,19 @@ import json
 
 def query_api_site():
 	base_url = 'https://thanos.nrp-nautilus.io/api/v1/'
-	query = 'rate(container_cpu_usage_seconds_total{namespace="wifire-quicfire"}[3h])'
+	# query = 'rate(container_cpu_usage_seconds_total{namespace="wifire-quicfire"}[3h])'
 	query = 'rate(node_cpu_seconds_total[1m])'
+	# query = ' sum(kube_pod_container_resource_requests{job="kube-state-metrics", cluster="", namespace="wifire-quicfire", resource="memory"})'
+	# query = 'sum(container_memory_working_set_bytes{job="kubelet", metrics_path="/metrics/cadvisor", cluster="", namespace="wifire-quicfire"})'
+	# query = 'sum(node_namespace_pod_container:container_cpu_usage_seconds_total:sum_irate{cluster="", namespace="wifire-quicfire"}) / sum(kube_pod_container_resource_limits{job="kube-state-metrics", cluster="", namespace="wifire-quicfire", resource="cpu"})'
+	# query = 'sum(node_namespace_pod_container:container_cpu_usage_seconds)'
 	endpoint = f'query?query={query}'
 	full_url = base_url + endpoint
 	cpu_data = requests.get(full_url)
 	return cpu_data
 
 def write_json(data):
-	with open("webscrape.json","w") as file:
+	with open("e.json","w") as file:
 		json.dump(data.json(),file)
 
 def read_json():
