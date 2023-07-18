5d6967ec1fb5ca9e303569c760aaacd5f68deaee
Devin Shende <devin.s.shende@gmail.com>
2023-07-18 16:28:30 -0700

    pandas dataframe starter code

--- main.py
+++ main.py
@@ -2,23 +2,17 @@ import json
 import requests
 from bs4 import BeautifulSoup
 from utils import *
+import pandas as pd
 
-print("\n\n\n\n")
-print_query_values(True)
-print("\n\n\n\n")
+print("\n\n")
+
+column_names=["CPU Usage","CPU Requests",'CPU Requests %', 'CPU Limits']
+data = [ ["Spark",20000, "30days", 0], 
+         ["Pandas",25000, "40days", 0], ]
+
+df = pd.DataFrame(data, columns=column_names)
 
 
-# json_print()
-
-
-# url = "https://grafana.nrp-nautilus.io/d/85a562078cdf77779eaa1add43ccec1e/kubernetes-compute-resources-namespace-pods?orgId=1&var-datasource=thanos&var-cluster=&var-namespace=wifire&from=1756419957757&to=1806913309834"
-# all_data = requests.get(url)
-
-# s = BeautifulSoup(all_data.content, "html.parser")
-# print(s)
-# print("successfully requested url")
-
-# print(s.text)
-# res = s.find(id="containername")
-# title = s.find("h2",class_="title is-5")
-# print(title[0].text)
+d = {'CPU Usage': 3, 'CPU Requests':3, 'CPU Requests %': 89, 'CPU Limits': 93}
+df = df.append(d, ignore_index = True)
+print(df)
