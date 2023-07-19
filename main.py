f6500e8a71d685b71e16f2714cd30678ffbbcc60
devinshende <34968827+devinshende@users.noreply.github.com>
2023-07-19 16:56:07 -0700

    Update main.py

--- main.py
+++ main.py
@@ -10,28 +10,25 @@ from network_usage import *
 column_names=["Pod","CPU Usage","CPU Requests",'CPU Requests %', 'CPU Limits']
 data = [ [None, None, None, None, None]]
 
-df = pd.DataFrame(columns=column_names)
-
-def get_pods_list():
-	data = query_api_site('rate(container_cpu_usage_seconds_total{namespace="wifire-quicfire"}[3h])')
-	try:
-		pods = data["data"]["result"]
-		pods_list = []
-		for pod in pods:
-			pods_list.append(pod["metric"]["pod"])
-	except KeyError as e:
-		return ["Error retrieving pods"]
-	return pods_list
+cpu_df = pd.DataFrame(columns=column_names)
+mem_df = pd.DataFrame(columns=column_names)
 
 pods_l = get_pods_list()
 
-
+cpu_quota = CPUQuota()
+mem_quota = MemQuota()
 for p in pods_l:
-	cpu_quota = CPUQuota()
-	a = cpu_quota.get_table_row(p)
-	print("\n\n\n", a, "\n\n\n")
-	df = df.append(a, ignore_index = True)
+    a = cpu_quota.get_table_row(p)
+    cpu_df = cpu_df.append(a, ignore_index = True)
+    b = mem_quota.get_table_row(p)
+    mem_df = mem_df.append(b, ignore_index=True)
 
 
-print(df)
+print(cpu_df)
+print("\n\n")
+print(mem_df)
+print("\n\n")
+print_query_values()
 print("\n\n\n")
+
+print("Total API pings: ",get_query_count())
