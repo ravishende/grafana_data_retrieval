f696e1e82db0db0872d20913207f0945383ee76b
Devin Shende <devin.s.shende@gmail.com>
2023-07-18 17:01:12 -0700

    new class

--- main.py
+++ main.py
@@ -10,16 +10,7 @@ data = [ [None, None, None, None]]
 
 df = pd.DataFrame(data, columns=column_names)
 
-def get_cpu_usage(pod):
-	cpu_usage = query_api_site('sum(node_namespace_pod_container:container_cpu_usage_seconds_total:sum_irate{cluster="", namespace="wifire-quicfire", pod="'+pod+'"})')
-	cpu_usage = cpu_usage.json()['data']['result']
-	
-	# if error retrieving from api, ping again recursively
-	if (len(cpu_usage) == 0):
-
-		return get_cpu_usage(pod)
-
-	return float(cpu_usage[0]['value'][1])
+M = MemQuota()
 
 cpu = round(get_cpu_usage('bp3d-rabbitmq-5845c99598-99rcn'),2)
 
