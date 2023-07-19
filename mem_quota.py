751da284b9b1a0bdd7201d8caf4f940e3ff38d47
Devin Shende <devin.s.shende@gmail.com>
2023-07-18 17:01:50 -0700

    moved it

--- mem_quota.py
+++ mem_quota.py
@@ -6,17 +6,6 @@ class MemQuota():
 	def __init__(self):
 		pass
 
-	def _get_cpu_usage(self,pod):
-		cpu_usage = query_api_site('sum(node_namespace_pod_container:container_cpu_usage_seconds_total:sum_irate{cluster="", namespace="wifire-quicfire", pod="'+pod+'"})')
-		cpu_usage = cpu_usage.json()['data']['result']
-		
-		# if error retrieving from api, ping again recursively
-		if (len(cpu_usage) == 0):
-
-			return get_cpu_usage(pod)
-
-		return float(cpu_usage[0]['value'][1])
-
 	def mem_quota(self, pod):
 		result = {
 			"CPU Usage":None, 
@@ -26,6 +15,16 @@ class MemQuota():
 		}
 		result["CPU Usage"] = self._get_cpu_requests()
 		return result
+
+	def _get_cpu_usage(self,pod):
+		cpu_usage = query_api_site('sum(node_namespace_pod_container:container_cpu_usage_seconds_total:sum_irate{cluster="", namespace="wifire-quicfire", pod="'+pod+'"})')
+		cpu_usage = cpu_usage.json()['data']['result']
+		
+		# if error retrieving from api, ping again recursively
+		if (len(cpu_usage) == 0):
+			return get_cpu_usage(pod)
+
+		return float(cpu_usage[0]['value'][1])
 
 	def _get_cpu_requests(self,pod):
 		pass
