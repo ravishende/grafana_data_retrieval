5706a06fde0d2654f13766dda5b6917a0a055621
Devin Shende <devin.s.shende@gmail.com>
2023-07-19 11:49:24 -0700

    ready to merge

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ df = pd.DataFrame(columns=column_names)
 def get_pods_list():
 	data = query_api_site('rate(container_cpu_usage_seconds_total{namespace="wifire-quicfire"}[3h])')
 	try:
-		pods = data.json()["data"]["result"]
+		pods = data["data"]["result"]
 		pods_list = []
 		for pod in pods:
 			pods_list.append(pod["metric"]["pod"])
