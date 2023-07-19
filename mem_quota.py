3fbb766c233ae046619189c1fe229b767f318c58
Devin Shende <devin.s.shende@gmail.com>
2023-07-18 17:14:36 -0700

    mem quota starter

--- mem_quota.py
+++ mem_quota.py
@@ -4,17 +4,19 @@ column_names=["CPU Usage","CPU Requests",'CPU Requests %', 'CPU Limits']
 
 class MemQuota():
 	def __init__(self):
-		pass
-
-	def mem_quota(self, pod):
-		result = {
+		self.result = {
 			"CPU Usage":None, 
 			"CPU Requests":None, 
 			'CPU Requests %':None, 
 			'CPU Limits':None
 		}
-		result["CPU Usage"] = self._get_cpu_requests()
-		return result
+
+	def get_values(self, pod):
+		self.result["CPU Usage"] = self._get_cpu_usage(pod)
+		self.result["CPU Requests"] = self._get_cpu_requests(pod)
+		self.result["CPU Requests %"] = self._get_cpu_requests_percent(pod)
+		self.result["CPU Limits"] = self._get_cpu_limits(pod)
+		return self.result
 
 	def _get_cpu_usage(self,pod):
 		cpu_usage = query_api_site('sum(node_namespace_pod_container:container_cpu_usage_seconds_total:sum_irate{cluster="", namespace="wifire-quicfire", pod="'+pod+'"})')
@@ -22,7 +24,7 @@ class MemQuota():
 		
 		# if error retrieving from api, ping again recursively
 		if (len(cpu_usage) == 0):
-			return get_cpu_usage(pod)
+			return self._get_cpu_usage(pod)
 
 		return float(cpu_usage[0]['value'][1])
 
