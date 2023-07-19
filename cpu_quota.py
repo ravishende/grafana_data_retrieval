44ffb39f5b1615c5c6ba3b4792d3677181642302
ravishende <ravi.sameer.shende@gmail.com>
2023-07-19 11:52:25 -0700

    refactored and simplified cpu_quota w/ new utils functions

--- cpu_quota.py
+++ cpu_quota.py
@@ -2,9 +2,11 @@ from utils import *
 
 column_names=["CPU Usage","CPU Requests",'CPU Requests %', 'CPU Limits']
 
+
 class CPUQuota():
 	def __init__(self):
 		self.result = {
+			"Pod": None,
 			"CPU Usage":None, 
 			"CPU Requests":None, 
 			"CPU Requests %":None, 
@@ -12,46 +14,28 @@ class CPUQuota():
 			"CPU Limits %": None
 		}
 
-	def get_values(self, pod):
-		self.result["CPU Usage"] = self._get_cpu_usage(pod)
-		self.result["CPU Requests"] = self._get_cpu_requests(pod)
-		self.result["CPU Requests %"] = self._get_cpu_requests_percent(pod)
-		self.result["CPU Limits"] = self._get_cpu_limits(pod)
-		self.result["CPU Limits %"] = self._get_cpu_limits_percent(pod)
+
+	def get_table_row(self, pod):
+		#assemble queries for the given pod
+		queries_dict = {
+			'CPU Usage':'sum(node_namespace_pod_container:container_cpu_usage_seconds_total:sum_irate{cluster="", namespace="wifire-quicfire", pod="'+pod+'"})',
+			'CPU Requests':'sum(cluster:namespace:pod_cpu:active:kube_pod_container_resource_requests{cluster="", namespace="wifire-quicfire", pod="'+pod+'"})',
+			'CPU Limits':'sum(cluster:namespace:pod_cpu:active:kube_pod_container_resource_limits{cluster="", namespace="wifire-quicfire", pod="'+pod+'"})'
+		}
+
+		#fill in self.result for non percentage keys
+		for query_title, value in self.result:
+			if query_title in queries_dict.keys():
+				queries_dict[query_title] = query_value(queries_dict[query_title])
+
+		#fill in self.result with percentages and pod
+		self.result["Pod"] = pod
+		self.result['CPU Requests %'] = _get_percent(self.result['CPU Usage'], self.result['CPU Requests'])
+		self.result['CPU Limits %'] = _get_percent(self.result['CPU Usage'], self.result['CPU Limits'])
+
+		
 		return self.result
 
-	def _get_cpu_usage(self,pod):
-		cpu_usage = query_api_site('sum(node_namespace_pod_container:container_cpu_usage_seconds_total:sum_irate{cluster="", namespace="wifire-quicfire", pod="'+pod+'"})')
-		cpu_usage = cpu_usage.json()['data']['result']
-		
-		# if error retrieving from api, ping again recursively
-		if (len(cpu_usage) == 0):
-			return self._get_cpu_usage(pod)
-
-		return float(cpu_usage[0]['value'][1])
-
-	def _get_cpu_requests(self,pod):
-		cpu_usage = query_api_site('sum(cluster:namespace:pod_cpu:active:kube_pod_container_resource_requests{cluster="", namespace="wifire-quicfire", pod="'+pod+'"})')
-		cpu_usage = cpu_usage.json()['data']['result']
-		
-		# if error retrieving from api, ping again recursively
-		if (len(cpu_usage) == 0):
-			return self._get_cpu_requests(pod)
-
-		return float(cpu_usage[0]['value'][1])
-
-	def _get_cpu_requests_percent(self, pod):
-		return self.result["CPU Usage"] / self.result["CPU Requests"]
-
-	def _get_cpu_limits(self,pod):
-		cpu_usage = query_api_site('sum(cluster:namespace:pod_cpu:active:kube_pod_container_resource_limits{cluster="", namespace="wifire-quicfire", pod="'+pod+'"})')
-		cpu_usage = cpu_usage.json()['data']['result']
-		
-		# if error retrieving from api, ping again recursively
-		if (len(cpu_usage) == 0):
-			return self._get_cpu_limits(pod)
-
-		return float(cpu_usage[0]['value'][1])
-
-	def _get_cpu_limits_percent(self,pod):
-		return self.result["CPU Usage"] / self.result["CPU Limits"]
+	#used to avoid any unnecessary queries to the database, instead calculating the percent on our own
+	def _get_percent(self, portion, total):
+		return round(portion/total, 3)*100
