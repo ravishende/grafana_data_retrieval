9120d8a73b5c896401dabbc223217df6639a0b3f
ravishende <ravi.sameer.shende@gmail.com>
2023-07-20 09:37:38 -0700

    added reusable functionality for getting df columns and rearranged new_queries.py

--- main.py
+++ main.py
@@ -7,16 +7,18 @@ from cpu_quota import *
 from mem_quota import *
 from network_usage import *
 
-column_names=["Pod","CPU Usage","CPU Requests",'CPU Requests %', 'CPU Limits']
+# column_names=["Pod","CPU Usage","CPU Requests",'CPU Requests %', 'CPU Limits']
 data = [ [None, None, None, None, None]]
-
-cpu_df = pd.DataFrame(columns=column_names)
-mem_df = pd.DataFrame(columns=column_names)
 
 pods_l = get_pods_list()
 
 cpu_quota = CPUQuota()
 mem_quota = MemQuota()
+
+cpu_df = pd.DataFrame(columns=get_column_names(cpu_quota))
+mem_df = pd.DataFrame(columns=get_column_names(mem_quota))
+
+
 for p in pods_l:
     a = cpu_quota.get_table_row(p)
     cpu_df = cpu_df.append(a, ignore_index = True)
