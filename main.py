51e9c934b8972c6cc9d820a47e1c99e128677f63
Devin Shende <devin.s.shende@gmail.com>
2023-07-20 09:40:07 -0700

    pickling and printing of dataframes

--- main.py
+++ main.py
@@ -10,25 +10,31 @@ from network_usage import *
 column_names=["Pod","CPU Usage","CPU Requests",'CPU Requests %', 'CPU Limits']
 data = [ [None, None, None, None, None]]
 
-cpu_df = pd.DataFrame(columns=column_names)
-mem_df = pd.DataFrame(columns=column_names)
+pd.set_option('display.max_columns', 7)
 
-pods_l = get_pods_list()
+# cpu_df = pd.DataFrame(columns=column_names)
+# mem_df = pd.DataFrame(columns=column_names)
+# pods_l = get_pods_list()
 
-cpu_quota = CPUQuota()
-mem_quota = MemQuota()
-for p in pods_l:
-    a = cpu_quota.get_table_row(p)
-    cpu_df = cpu_df.append(a, ignore_index = True)
-    b = mem_quota.get_table_row(p)
-    mem_df = mem_df.append(b, ignore_index=True)
+# cpu_quota = CPUQuota()
+# mem_quota = MemQuota()
+# for p in pods_l:
+#     a = cpu_quota.get_table_row(p)
+#     cpu_df = cpu_df.append(a, ignore_index = True)
+#     b = mem_quota.get_table_row(p)
+#     mem_df = mem_df.append(b, ignore_index=True)
 
+# mem_df.to_pickle("mem_df.pkl")
+# cpu_df.to_pickle("cpu_df.pkl")
+
+mem_df = pd.read_pickle("mem_df.pkl")  
+cpu_df = pd.read_pickle("cpu_df.pkl")
 
 print(cpu_df)
 print("\n\n")
 print(mem_df)
 print("\n\n")
-print_query_values()
+# print_query_values()
 print("\n\n\n")
 
 print("Total API pings: ",get_query_count())
