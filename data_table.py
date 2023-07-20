04cf8d3ed1262caea26ef28110c8290ee374238d
Devin Shende <devin.s.shende@gmail.com>
2023-07-20 13:08:53 -0700

    broken network function

--- data_table.py
+++ data_table.py
@@ -55,4 +55,32 @@ class DataTable():
 
 
 	def network_quota(self, duration):
-		pass
+
+		col_names = ["Pod","Current Receive Bandwidth", "Current Transmit Bandwidth", "Rate of Received Packets", "Rate of Transmitted Packets", "Rate of Received Packets Dropped", "Rate of Transmitted Packets"]
+
+		#assemble queries for the given pod
+		queries_dict = {
+			"Current Receive Bandwidth":'sum(irate(container_network_receive_bytes_total{job="kubelet", metrics_path="/metrics/cadvisor", cluster="", namespace="wifire-quicfire", pod="' + pod + '"}[' + ts + ']))',
+			"Current Transmit Bandwidth":'sum(irate(container_network_transmit_bytes_total{job="kubelet", metrics_path="/metrics/cadvisor", cluster="", namespace="wifire-quicfire", pod="' + pod + '"}[' + ts + ']))',
+			"Rate of Received Packets":'sum(irate(container_network_receive_packets_total{job="kubelet", metrics_path="/metrics/cadvisor", cluster="", namespace="wifire-quicfire", pod="' + pod + '"}[' + ts + ']))',
+			"Rate of Transmitted Packets":'sum(irate(container_network_transmit_packets_total{job="kubelet", metrics_path="/metrics/cadvisor", cluster="", namespace="wifire-quicfire", pod="' + pod + '"}[' + ts + ']))',
+			"Rate of Received Packets Dropped":'sum(irate(container_network_receive_packets_dropped_total{job="kubelet", metrics_path="/metrics/cadvisor", cluster="", namespace="wifire-quicfire", pod="' + pod + '"}[' + ts + ']))',	
+			"Rate of Transmitted Packets":'sum(irate(container_network_transmit_packets_dropped_total{job="kubelet", metrics_path="/metrics/cadvisor", cluster="", namespace="wifire-quicfire", pod="' + pod + '"}[' + ts + ']))'
+		}
+
+		#fill in self.result
+		self.result["Pod"] = pod
+		for query_title, query in queries_dict.items():
+			self.result[query_title] = query_value(query)
+
+		
+		return self.result
+
+
+
+
+
+
+
+
+
