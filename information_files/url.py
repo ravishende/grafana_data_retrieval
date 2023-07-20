72c198f9113166b23fd9b2c8c1854768bc9bdff4
ravishende <ravi.sameer.shende@gmail.com>
2023-07-20 09:06:20 -0700

    added folder for storing non-coding files

---
+++ information_files/url.py
@@ -0,0 +1,4 @@
+url_base = "https://grafana.nrp-nautilus.io/d/85a562078cdf77779eaa1add43ccec1e/kubernetes-compute-resources-namespace-pods?orgId=1&var-datasource=default&var-cluster="
+namespace = "&var-namespace=wifire-quicfire"
+time_frame = "&from=now-15m&to=now"
+
