e0dcf732c8d08ef15108a5dbfa37ed76cd76ac30
ravishende <ravi.sameer.shende@gmail.com>
2023-07-27 16:43:36 -0700

    fixed bug in graphs displays

--- inputs.py
+++ inputs.py
@@ -3,12 +3,12 @@ NAMESPACE = 'wifire-quicfire'
 # NAMESPACE = 'alto'
 
 #duration for current_network_usage table
-DEFAULT_DURATION = '5m'
+DEFAULT_DURATION = '10m'
 
 #rounding
 DEFAULT_ROUND_TO = 3
 ROUND_TABLES_TO = 5
 
 #graphs
-DEFAULT_GRAPH_STEP = '10m'
-DEFAULT_GRAPH_TIME_OFFSET = '3h'
+DEFAULT_GRAPH_STEP = '20m'
+DEFAULT_GRAPH_TIME_OFFSET = '6h'
