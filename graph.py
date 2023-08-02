b3a2359dda5732f31f66bc8d82c14cec0e8ddd11
ravishende <ravi.sameer.shende@gmail.com>
2023-08-02 14:57:21 -0700

    refactored graph.py to use dataframes. Also  changed test_graphs

--- graph.py
+++ graph.py
@@ -9,7 +9,7 @@ from datetime import datetime, timedelta
 class Graph():
 
 	def __init__(self, namespace=NAMESPACE, end=datetime.now(), duration=DEFAULT_DURATION, time_offset=DEFAULT_GRAPH_TIME_OFFSET, time_step=DEFAULT_GRAPH_STEP):
-		#define variables for querying data for graphs
+		#variables for querying data for graphs
 		self.namespace = namespace
 		self.end = end
 		self.duration = duration
@@ -42,6 +42,9 @@ class Graph():
 			]
 		}
 
+		#dataframe storing all the data for the graphs
+		# self.graphs_df = pd.DataFrame(data={}, columns=self.queries_dict.keys().join(self.partial_queries_dict.keys()))
+		self.graphs_df = pd.DataFrame()
 
 
 
@@ -71,80 +74,129 @@ class Graph():
 
 
 	#get a list of all the values and add a column for timestamps
-	def _generate_formated_result_list(self, query, display_time_as):
+	def _generate_graph_data(self, query):
 		#create time filter to then generate list of all datapoints for the graph
 		time_filter = self._assemble_time_filter()
 		result_list = get_result_list(query_api_site_for_graph(query, time_filter))
 		
-		#loop through the data for each pod
+		df_graph_times_list = []
+		df_graph_values_list = []
+		df_graph_pods_list = []
+
+
+		#loop through pods
 		for i in range(len(result_list)):
-			values_list = result_list[i]['values']
-		
-			#go through the values list of each pod and clean up time and value
-			for j in range(len(result_list[i]['values'])):
-				#round values
-				values_list[j][1] = clean_round(float(values_list[j][1]))
+			#prepare data to be extracted
+			times_values_list = result_list[i]['values']	
+			
+			df_values_list = []
+			df_times_list = []
+			
+			#fill in lists
+			for time_value_pair in times_values_list:
+				df_times_list.append(time_value_pair[0])
+				df_values_list.append(float(time_value_pair[1]))
+			df_pod_list = [result_list[i]['metric']['pod']]*len(df_times_list)
+			
+			#add pod's lists to the whole graph's lists
+			df_graph_times_list.extend(df_times_list)
+			df_graph_values_list.extend(df_values_list)
+			df_graph_pods_list.extend(df_pod_list)
 
-				#display the time in each datapoint how the user specified.
-				#the time is queried in seconds since the epoch (01/01/1970) so if user wants to display as seconds, do nothing.
-				if display_time_as != "seconds":
-					#get seconds to a datetime that can be used 
-					date_time = datetime.fromtimestamp(values_list[j][0])
+			
+			# #go through the values list of each pod and clean up time and value
+			# for j in range(len(times_values_list)):
+			# 	#round values
+			# 	times_values_list[j][1] = clean_round(float(times_values_list[j][1]))
+
+			# 	#display the time in each datapoint how the user specified.
+			# 	#the time is queried in seconds since the epoch (01/01/1970) so if user wants to display as seconds, do nothing.
+			# 	if display_time_as != "seconds":
+			# 		#get seconds to a datetime that can be used 
+			# 		date_time = datetime.fromtimestamp(times_values_list[j][0])
 					
-					if display_time_as == "datetime":
-						#Display timestamps as datetimes
-						values_list[j][0] = date_time
+			# 		if display_time_as == "datetime":
+			# 			#Display timestamps as datetimes
+			# 			times_values_list[j][0] = date_time
 
-					elif display_time_as == "timestamp":
-						#formats the time as a string for printing in a more readable way
-						values_list[j][0] = (date_time.strftime("%Y-%m-%d %H:%M:%S.%f"))
+			# 		elif display_time_as == "timestamp":
+			# 			#formats the time as a string for printing in a more readable way
+			# 			times_values_list[j][0] = (date_time.strftime("%Y-%m-%d %H:%M:%S.%f"))
 
-					else:
-						#input was not one of the options. 
-						raise ValueError('display_time_as can only be set to one of the following: "datetime", "timestamp", or "seconds".')
+			# 		else:
+			# 			#input was not one of the options. 
+			# 			raise ValueError('display_time_as can only be set to one of the following: "datetime", "timestamp", or "seconds".')
+
+			# # make sure the result list that is returned has the new updates
+			# result_list[i]['values'] = times_values_list
+
+		return df_graph_times_list, df_graph_values_list, df_graph_pods_list
 
 
-			#make sure the result list that is returned has the new updates
-			result_list[i]['values'] = values_list
 
-		return result_list
+	# #get a dictionary in the form of {graph titles: list of graph data}
+	# def get_graphs(self, display_time_as=DEFAULT_GRAPH_DISPLAY_TIME):
+	# 	#get all of the initial graphs from the normal queries		
+	# 	graphs_dict = {}
+	# 	for query_title, query in self.queries_dict.items():
+	# 		graphs_dict[query_title] = self._generate_formated_result_list(query, display_time_as=display_time_as)
+		
+	# 	#combine the partial queries in partial_queries_dict so that each list of two queries is one datapoint in graphs_dict
+	# 	for query_title, query_pair in self.partial_queries_dict.items():
+			
+	# 		#store the two queries' values
+	# 		read_data = self._generate_formated_result_list(query_pair[0], display_time_as=display_time_as)
+	# 		write_data = self._generate_formated_result_list(query_pair[1], display_time_as=display_time_as)
+			
+	# 		#loop through each dataset of a pod
+	# 		for pod_i in range(len(read_data)):
+	# 			#loop through the data points in a pod's graph
+	# 			for datapoint_i in range(len(read_data[pod_i]['values'])):
+	# 				#add the write_data value to the read_data value for each datapoint in graphs_dict[]
+	# 				read_data[pod_i]['values'][datapoint_i][1] += write_data[pod_i]['values'][datapoint_i][1]
 
+	# 		#put the newly modified read_data (which is now read+write data) into graphs_dict.
+	# 		graphs_dict[query_title] = read_data
 
+	# 	return graphs_dict
 
 	#get a dictionary in the form of {graph titles: list of graph data}
-	def get_graphs(self, display_time_as=DEFAULT_GRAPH_DISPLAY_TIME):
+	def _generate_graphs(self):
+		graphs = []
 		#get all of the initial graphs from the normal queries
-		graphs_dict = {}
 		for query_title, query in self.queries_dict.items():
-			graphs_dict[query_title] = self._generate_formated_result_list(query, display_time_as=display_time_as)
-		
-		#combine the partial queries in partial_queries_dict so that each list of two queries is one datapoint in graphs_dict
+			#collect graph data
+			times, values, pods = self._generate_graph_data(query)
+			#make and populate dataframe, then add to graphs
+			graph_df = pd.DataFrame()
+			graph_df['Time'] = times
+			graph_df['Pod'] = pods
+			graph_df[query_title] = values
+			graphs.append(graph_df)
+		#get graphs from partial queries
 		for query_title, query_pair in self.partial_queries_dict.items():
-			
 			#store the two queries' values
-			read_data = self._generate_formated_result_list(query_pair[0], display_time_as=display_time_as)
-			write_data = self._generate_formated_result_list(query_pair[1], display_time_as=display_time_as)
+			times, read_values, pods = self._generate_graph_data(query_pair[0])
+			write_values = self._generate_graph_data(query_pair[1])[1]
 			
-			#loop through each dataset of a pod
-			for pod_i in range(len(read_data)):
-				#loop through the data points in a pod's graph
-				for datapoint_i in range(len(read_data[pod_i]['values'])):
-					#add the write_data value to the read_data value for each datapoint in graphs_dict[]
-					read_data[pod_i]['values'][datapoint_i][1] += write_data[pod_i]['values'][datapoint_i][1]
-
+			graph_df = pd.DataFrame()
+			graph_df['Time'] = times
+			graph_df['Pod'] = pods
+			graph_df[query_title] = [read_vals + write_vals for read_vals, write_vals in zip(read_values, write_values)]
+			
 			#put the newly modified read_data (which is now read+write data) into graphs_dict.
-			graphs_dict[query_title] = read_data
+			graphs.append(graph_df)
+		return graphs
 
-		return graphs_dict
 
-	#print the values list received from _generate_formated_result_list for each graph
-	def print_graphs(self, display_time_as=DEFAULT_GRAPH_DISPLAY_TIME):
-		graphs = self.get_graphs(display_time_as)
-		for graph_title, graph_data in graphs.items():
-			print("\n\n____________________________________________________") 
-			print("\t", colored(graph_title, 'green'))
-			print("____________________________________________________") 
-			print_by_pod(graph_data)
-			print("\n\n\n")
 
+	def get_graphs(self, display_time_as_timestamp=True):
+		graphs = self._generate_graphs()
+		for graph in graphs:
+			#update graphs with correct time columns
+			if display_time_as_timestamp:
+				graph['Time'] = pd.to_datetime(graph['Time'], unit="s")
+			#after time is in correct format, set it to be the index.
+			graph.set_index('Time', inplace=True)
+		return graphs
 
--- test_graphs.py
+++ test_graphs.py
@@ -7,34 +7,53 @@ from datetime import datetime, timedelta
 import matplotlib.pyplot as plt
 import time
 
-def display_graphs(graph_class):
-	graph_list = graph_class.get_graphs(display_time_as='timestamp').items()
+
+def print_graphs(graphs, only_print_worker_pods=False):
+	for graph in graphs:
+		#split data by pod
+		print("\n\n\n\n")
+		graph_groups = graph.groupby('Pod')
+
+		#print out graph of each pod
+		for pod in graph['Pod'].unique():
+			worker_id = get_worker_id(pod)
+			if only_print_worker_pods:
+				if worker_id != None:
+					print("\n\n\n")
+					printc(graph_groups.get_group(pod))
+			else:
+				print("\n\n\n")
+				printc(graph_groups.get_group(pod))
+
+# def display_graphs(graph_class):
+	# graph_list = graph_class.get_graphs(display_time_as_timestamp=True)
 
 
 	#display graphs
-	for graph_title, graph_data in graph_list:
-		
-		#collect the data from the graph
-		for pod_json in graph_data:
-			if get_worker_id(pod_json["metric"]["pod"]) != None:
-				pod = pod_json['metric']['pod']
-				df = pd.DataFrame(pod_json['values'], columns = ["time","value"])
-				plt.plot(df["time"],df["value"], label = pod_json["metric"]["pod"])
-				plt.xticks(df["time"],rotation=80)
+
+	# 	#collect the data from the graph
+	# 	# # for pod_json in graph_df:
+	# 	# 	if get_worker_id(graph_df[pod]) != None:
+	# 	# 		# pod = pod_json['metric']['pod']
+	# 	# 		# df = pd.DataFrame(pod_json['values'], columns = ["time","value"])
+	# 	for pod in graph_df['Pod']:
+
+	# 			plt.plot(df["time"],df["value"], label = pod_json["metric"]["pod"])
+	# 			plt.xticks(df["time"],rotation=80)
 
 		#put data into a numpy readable format
 		# y_np_list = np.array(y_data, dtype=np.float64)
-		plt.xlabel("Time")
-		plt.ylabel(graph_title)
-		plt.title(f'{graph_title} over time')
-		plt.legend(loc=3, prop={'size': 8})
-		plt.show()
+		# plt.xlabel("Time")
+		# plt.ylabel(graph_title)
+		# plt.title(f'{graph_title} over time')
+		# plt.legend(loc=3, prop={'size': 8})
+		# plt.show()
 
-		#for showing the next graph after the user closes the current one
-		time.sleep(0.2)
-		plt.close()
+		# #for showing the next graph after the user closes the current one
+		# time.sleep(0.2)
+		# plt.close()
 
 #Either run display_graphs(graph_class) or graph_class.print_graphs() depending on if you want data visualized or just printed
 graph_class = Graph()
-graph_class.print_graphs()
-# display_graphs(graph_class)
+graphs = graph_class.get_graphs()
+print_graphs(graphs, only_print_worker_pods=False)
