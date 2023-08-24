d766e27d162e2e54bfff27222df68261108495a8
ravishende <ravi.sameer.shende@gmail.com>
2023-08-24 14:44:46 -0700

    added functionality for nodes with graphs

--- basic_queries_testing/app.py
+++ basic_queries_testing/app.py
@@ -1,12 +1,15 @@
 from header import *
 from tables import *
+from graphs import *
 from termcolor import cprint, colored
 
 
 print(f'\n\n\n\n{colored("Header:", "magenta")}')
 headers = Header()
 tables = Tables()
+graphs = Graphs()
 headers.print_header_data()
 # tables.check_success()
+graphs.print_graph_data()
 
 
