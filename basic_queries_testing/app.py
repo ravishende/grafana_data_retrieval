3c77866e4d2c826f1a0da4a9637525f9c975c5c8
ravishende <ravi.sameer.shende@gmail.com>
2023-08-24 21:02:59 -0700

    got tables organized into proper tables instead of a separate df per column

--- basic_queries_testing/app.py
+++ basic_queries_testing/app.py
@@ -4,12 +4,13 @@ from graphs import *
 from termcolor import cprint, colored
 
 
-print(f'\n\n\n\n{colored("Header:", "magenta")}')
+# print(f'\n\n\n\n{colored("Header:", "magenta")}')
 headers = Header()
 tables = Tables()
 graphs = Graphs()
-headers.print_header_data()
+# headers.print_header_data()
 # tables.check_success()
-graphs.print_graph_data()
+print("\n\n\n", tables.print_tables(), "\n\n\n")
+# graphs.print_graph_data()
 
 
