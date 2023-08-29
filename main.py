a5e60e1324dce39935776f0c03eb3aa28f2a9afd
Devin Shende <devin.s.shende@gmail.com>
2023-08-29 12:19:36 -0700

    pep8 linting

--- main.py
+++ main.py
@@ -1,66 +1,70 @@
-from header import *
-from tables import *
-from graphs import *
+from header import Header
+from tables import Tables
+from graphs import Graphs
 from termcolor import colored
-from IPython.display import display
+import pandas as pd
 
-#set pandas display dataframe options
+# set pandas display dataframe options
 pd.set_option('display.max_columns', None)
 pd.set_option('display.expand_frame_repr', False)
 
-#create variables for classes
+# create variables for classes
 header_class = Header()
 tables_class = Tables()
 graphs_class = Graphs()
 
 
-# returns three dicts: one containing all header data, one with all tables, and one with all graph data
+# returns three dicts: one containing all header data,
+# one with all tables, and one with all graph data
 def get_all_data():
 	print("    Retrieving Header Data")
 	header_dict = header_class.get_header_dict()
-	
+
 	print("    Retrieving Tables Data")
 	tables_dict = tables_class.get_tables_dict()
-	
+
 	print("    Retrieving Graphs Data")
 	graphs_dict = graphs_class.get_graphs_dict(
-		display_time_as_timestamp=True, 
-		only_include_worker_pods=False, 
+		display_time_as_timestamp=True,
+		only_include_worker_pods=False,
 		show_runtimes=False
 	)
 
 	return header_dict, tables_dict, graphs_dict
 
-#Helper Function: for a given dictionary in the form {titles:dataframes}, print the title and dataframe of each item in the dict
+
+# Helper Function: for a given dictionary in the form {titles:dataframes}
+# print the title and dataframe of each item in the dict
 def print_dict(dictionary):
 	for title, dataframe in dictionary.items():
-		print("\n\n______________________________________________________________________________\n")
+		print("\n\n", "-"*100, "\n")
 		print("            ", colored(title, "green"))
-		print("______________________________________________________________________________\n")
+		print("-" * 100, "\n")
 		if len(dataframe.index) > 0:
 			print(dataframe)
 		else:
-			print(colored("No Data", "red")) 
+			print(colored("No Data", "red"))
 		print("\n\n")
 
-#prints data for headers, tables, and graphs.
+
+# prints data for headers, tables, and graphs.
 def print_all_data(header_dict=None, tables_dict=None, graphs_dict=None):
-	if header_dict==None or tables_dict==None or graphs_dict==None:
+	if header_dict is None or tables_dict is None or graphs_dict is None:
 		header_dict, tables_dict, graphs_dict = get_all_data()
 
-	print("\n\n\n\n********************************************************************************")
+	print("\n\n\n\n", "*"*100)
 	print(colored("                Header:", "magenta"))
-	print("********************************************************************************")
+	print("*" * 100)
 	print_dict(header_dict)
 
-	print("\n\n\n\n********************************************************************************")
+	print("\n\n\n\n", "*"*100)
 	print(colored("                Tables:", "magenta"))
-	print("********************************************************************************")
+	print("*" * 100)
 	print_dict(tables_dict)
 
-	print("\n\n\n\n********************************************************************************")
+	print("\n\n\n\n", "*"*100)
 	print(colored("                Graphs:", "magenta"))
-	print("********************************************************************************")
+	print("*" * 100)
 	print_dict(graphs_dict)
 
 
--- utils.py
+++ utils.py
@@ -2,126 +2,126 @@
 # -*- coding: utf-8 -*-
 import requests
 import json
-from inputs import *
-from decimal import *
 import re
-from datetime import datetime, timedelta
-from termcolor import cprint, colored
-from pprint import pprint
-from rich import print as printc
+from termcolor import colored
 
-#reset total query count
+# reset total query count
 QUERY_COUNT = 0
 
-#retrieves global variable for total number of querries since the start of the program
+
+# retrieves global variable for total number of querries
+# since the start of the program
 def get_query_count():
-	global QUERY_COUNT
-	return QUERY_COUNT
+    global QUERY_COUNT
+    return QUERY_COUNT
+
 
 # Use url and a given query to request data from the website
 def query_api_site(query, handle_fail=True):
-	# handle fail will re request the api if it gets no response from your query. Set to true by default
-	# there is a bug with the api itself where every fifth request comes back with no data, 
-	# this parameter set to True will re request to deal with that	
-	
-	global QUERY_COUNT
-	#set up url
-	base_url = 'https://thanos.nrp-nautilus.io/api/v1/'
-	endpoint = f'query?query={query}'
-	full_url = base_url + endpoint
-	#query database
-	queried_data = requests.get(full_url).json()
-	QUERY_COUNT += 1
+    # handle fail will re request the api if no response from query. Set to true by default
+    # there is a bug with the api itself where every fifth request comes back with no data,
+    # this parameter set to True will re request to deal with that
 
-	#re-request data if it comes back with no value
-	if handle_fail:
-		try:
-			res_list = get_result_list(queried_data)
-			if len(res_list) == 0:
-				queried_data = requests.get(full_url).json()
-		except KeyError:
-			print(f'\n\nqueried_data is\n{colored(queried_data,"red")}\n')
-			raise TypeError(f'\n\nBad query string: {query}\n\n')
+    global QUERY_COUNT
+    # set up url
+    base_url = "https://thanos.nrp-nautilus.io/api/v1/"
+    endpoint = f"query?query={query}"
+    full_url = base_url + endpoint
+    # query database
+    queried_data = requests.get(full_url).json()
+    QUERY_COUNT += 1
 
-	return queried_data
+    # re-request data if it comes back with no value
+    if handle_fail:
+        try:
+            res_list = get_result_list(queried_data)
+            if len(res_list) == 0:
+                queried_data = requests.get(full_url).json()
+        except KeyError:
+            print(f'\n\nqueried_data is\n{colored(queried_data,"red")}\n')
+            raise TypeError(f'\n\nBad query string: {query}\n\n')
+
+    return queried_data
 
 
 # Use url and a given query and time_filter to request data for a graph from the api
 # Different function from query_api_site() to avoid confusion with querying single data points and tables vs graphs
 def query_api_site_for_graph(query, time_filter, handle_fail=True):
-	# handle fail will re request the api if it gets no response from your query. Set to true by default
-	# there is a bug with the api itself where every fifth request comes back with no data, 
-	# this parameter set to True will re request to deal with that	
+    # handle fail will re request the api if it gets no response from your query. Set to true by default
+    # there is a bug with the api itself where every fifth request comes back with no data,
+    # this parameter set to True will re request to deal with that
 
-	global QUERY_COUNT
-	#set up url
-	base_url = 'https://thanos.nrp-nautilus.io/api/v1/'
-	endpoint = f'query_range?query={query}&{time_filter}'
-	full_url = base_url + endpoint
-	#query database
-	queried_data = requests.get(full_url).json()
-	QUERY_COUNT += 1
+    global QUERY_COUNT
+    # set up url
+    base_url = 'https://thanos.nrp-nautilus.io/api/v1/'
+    endpoint = f'query_range?query={query}&{time_filter}'
+    full_url = base_url + endpoint
+    # query database
+    queried_data = requests.get(full_url).json()
+    QUERY_COUNT += 1
 
-	#re-request data if it comes back with no value
-	if (handle_fail):
-		try:
-			res_list = get_result_list(queried_data)
-			if (len(res_list) == 0):
-				queried_data = requests.get(full_url).json()
-		except KeyError:
-			print(f'\n\nqueried_data is\n{colored(queried_data,"red")}\n')
-			raise TypeError (f'\n\nBad query string: \n{full_url}\n\n')
+    # re-request data if it comes back with no value
+    if (handle_fail):
+        try:
+            res_list = get_result_list(queried_data)
+            if (len(res_list) == 0):
+                queried_data = requests.get(full_url).json()
+        except KeyError:
+            print(f'\n\nqueried_data is\n{colored(queried_data,"red")}\n')
+            raise TypeError(f'\n\nBad query string: \n{full_url}\n\n')
 
-	return queried_data
+    return queried_data
 
 
-
-#given json data from querying the api, retrieve the result of the query as a list of two floats
+# given json data from querying the api, retrieve the result
+# of the query as a list of two floats
 def get_result_list(api_response):
-	return api_response['data']['result']
+    return api_response['data']['result']
 
 
-#given a string in the form 5w3d6h30m5s, save the times to a dict accesible by the unit as their key. The int times can be any length (500m160s is allowed)
-#works given as many or few of the time units. (e.g. 12h also works and sets everything but h to None)
+# given a string in the form 5w3d6h30m5s, save the times to a dict accesible
+# by the unit as their key. The int times can be any length (500m160s is allowed)
+# works given as many or few of the time units. (e.g. 12h also works and sets everything but h to None)
 def get_time_dict_from_str(time_str):
-	#define regex pattern (groups by optional int+unit but only keeps the int)
-	pattern = '(?:(\d+)w)?(?:(\d+)d)?(?:(\d+)h)?(?:(\d+)m)?(?:(\d+)s)?'
-	feedback = re.search(pattern, time_str)
+    # define regex pattern (groups by optional int+unit but only keeps the int)
+    pattern = "(?:(\d+)w)?(?:(\d+)d)?(?:(\d+)h)?(?:(\d+)m)?(?:(\d+)s)?"
+    feedback = re.search(pattern, time_str)
 
-	#save time variables (if not in time_str they will be set to None)
-	w, d, h, m, s = feedback.groups()
-	#put time variables into a dictionary
-	time_dict = {
-		'weeks':w,
-		'days':d,
-		'hours':h,
-		'minutes':m,
-		'seconds':s
-	}
+    # save time variables (if not in time_str they will be set to None)
+    w, d, h, m, s = feedback.groups()
+    # put time variables into a dictionary
+    time_dict = {
+        'weeks': w,
+        'days': d,
+        'hours': h,
+        'minutes': m,
+        'seconds': s
+    }
 
-	#get rid of null values in time_dict
-	time_dict = {unit:float(value) for unit, value in time_dict.items() if value != None}
-	
-	return time_dict
+    # get rid of null values in time_dict
+    time_dict = {unit: float(value) for unit, value in time_dict.items() if value is not None}
+
+    return time_dict
+
 
 def get_worker_id(pod_name):
-	worker_title = 'bp3d-worker-k8s-'
-	#if pod_name is a bp3d worker, return the ensemble
-	title_len = len(worker_title)
-	if(pod_name[0:title_len] == worker_title):
-		return pod_name[title_len:-1]
-	#otherwise, return None
-	return None
+    worker_title = 'bp3d-worker-k8s-'
+    # if pod_name is a bp3d worker, return the ensemble
+    title_len = len(worker_title)
+    if pod_name[0:title_len] == worker_title:
+        return pod_name[title_len:-1]
+    # otherwise, return None
+    return None
 
 
-#writes json data to a file
+# writes json data to a file
 def write_json(data):
-	with open('e.json', 'w') as file:
-		json.dump(data.json(), file)
+    with open('e.json', 'w') as file:
+        json.dump(data.json(), file)
 
 
-#reads json data from a file
+# reads json data from a file
 def read_json():
-	with open('scrape.json', 'r') as file:
-		data = json.load(file)
-	return data
+    with open('scrape.json', 'r') as file:
+        data = json.load(file)
+    return data
