be23c7411d7ee7bb1510aafe24ff0dd4292c0513
ravishende <ravi.sameer.shende@gmail.com>
2023-09-06 03:20:50 -0700

    added more explanation to queries_notes.md

--- inputs.py
+++ inputs.py
@@ -2,8 +2,8 @@
 # -*- coding: utf-8 -*-
 
 # namespace
-# NAMESPACE = 'wifire-quicfire'
-NAMESPACE = 'alto'  # used for testing when no data in wifire-quicfire
+NAMESPACE = 'wifire-quicfire'
+# NAMESPACE = 'alto'  # used for testing when no data in wifire-quicfire
 
 # duration for all 3 data types: Header, tables, graphs
 DEFAULT_DURATION = '10m'
@@ -11,4 +11,5 @@ DEFAULT_DURATION = '10m'
 # graphs
 DEFAULT_GRAPH_STEP = '1m'
 DEFAULT_GRAPH_TIME_OFFSET = '30m'
+# requerying
 REQUERY_GRAPH_STEP_DIVISOR = 10  # a value of 10 means there will be 10x the number of datapoints for a given timeframe
