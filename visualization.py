5e4adda2ad9a345e7835c03d53c3c615f9b9d306
ravishende <ravi.sameer.shende@gmail.com>
2023-09-07 21:13:19 -0700

    added ability to display all graphs in vizualitzations.py

--- visualization.py
+++ visualization.py
@@ -2,23 +2,30 @@ import pandas as pd
 import seaborn as sns
 import time
 import matplotlib.pyplot as plt
+from graphs import Graphs
 from datetime import datetime as dt
 
-df = pd.read_csv("cpu_usage.csv")
+graphs_class = Graphs()
+graphs_dict = graphs_class.get_graphs_dict(display_time_as_timestamp=False)
 
-tick_spacing = 1
+for graph_title, graph_df in graphs_dict.items():
+# graph_df = pd.read_csv("cpu_usage.csv")
 
-time_0 = df["Time"][0]
-df["Time"] = df.apply(lambda row: row["Time"]-time_0,axis=1)
-my_datetime = dt.fromtimestamp(time_0)
-print(my_datetime)
-s = my_datetime.strftime("%H:%M")
+    tick_spacing = 1
 
-ax = sns.lineplot(data=df, x="Time", y="Rate of Transmitted Packets",hue="Pod")
-# ax.set(xlim=0)
-# labels = ["13:44", "2023-08-30 13:50:42.138000128", "2023-08-30 13:55:42.138000128"]
-# ax.set(xticklabels=labels)
-plt.title("Rate of Transmitted Packets")
-plt.xlabel("Seconds Since "+s)
-plt.xticks(rotation = 25)
-plt.show()
+    if graph_df is None:
+        continue
+    time_0 = graph_df["Time"][0]
+    graph_df["Time"] = graph_df.apply(lambda row: row["Time"]-time_0, axis=1)
+    my_datetime = dt.fromtimestamp(time_0)
+    print(my_datetime)
+    s = my_datetime.strftime("%H:%M")
+
+    ax = sns.lineplot(data=graph_df, x="Time", y=graph_title,hue="Pod")
+    # ax.set(xlim=0)
+    # labels = ["13:44", "2023-08-30 13:50:42.138000128", "2023-08-30 13:55:42.138000128"]
+    # ax.set(xticklabels=labels)
+    plt.title(graph_title)
+    plt.xlabel("Seconds Since "+s)
+    plt.xticks(rotation = 25)
+    plt.show()
