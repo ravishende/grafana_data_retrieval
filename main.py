3ff244b49b3f0a5a7c85d76c3d959abe82aeb2fd
ravishende <ravi.sameer.shende@gmail.com>
2023-09-13 18:08:27 -0700

    dealt with edge cases of no data while printing requeried graphs

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 from main_functions import *
+from utils import write_json
 import pandas as pd
 
 # set pandas display dataframe options to display all columns
@@ -11,7 +12,7 @@ pd.set_option('display.precision', 3)  # for how many decimal places to display 
 
 # run all code
 result_dict = get_all_data(
-    only_include_worker_pods=False,  # filters graphs for only worker pods
+    only_include_worker_pods=False,  # filters graphs for only worker pods. Updates pod names to be just their ensemble
     display_time_as_timestamp=True,  # displays time in readable format instead of seconds since epoch (01/01/1970)
     show_graph_runtimes=False,  # for displaying in the terminal how long each query and graph creation takes
     get_graphs_as_one_df=False  # puts all graphs into one dataframe instead of a dictionary with multiple graphs. For inputting into database in future
@@ -25,8 +26,8 @@ graphs = result_dict['graphs']
 losses_and_requeried_graphs = \
     check_graphs_losses(
         graphs=graphs,  # if this is not passed in, it will automatically be regenerated. This may take time
-        print_info=False,  # prints information on pods dropped/recovered: pod, values dropped/recovered, previous time, time of drop/recovery
-        requery=False,  # requeries pods dropped/recovered at higher resolution if True, doesn't if False. When None, prompts user if it should requery after collecting info
+        print_info=True,  # prints information on pods dropped/recovered: pod, values dropped/recovered, previous time, time of drop/recovery
+        requery=None,  # requeries pods dropped/recovered at higher resolution if True, doesn't if False. When None, prompts user if it should requery after collecting info
         show_runtimes=False,  # for displaying in the terminal how long each query and graph creation takes
         display_time_as_timestamp=True  # displays time in readable format instead of seconds since epoch (01/01/1970)
     )
