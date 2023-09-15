aff77d693cfde52181773b64e4caf72b0abc2258
ravishende <ravi.sameer.shende@gmail.com>
2023-09-15 11:15:02 -0700

    added comments to and fixed typo in graph_visualization.py

--- graph_visualization.py
+++ graph_visualization.py
@@ -3,29 +3,29 @@ import matplotlib.pyplot as plt
 from graphs import Graphs
 from datetime import datetime as dt
 
+# generate graphs dataframes
 graphs_class = Graphs()
 graphs_dict = graphs_class.get_graphs_dict(
     only_include_worker_pods=False,  # filters graphs to only include bpd3-worker pods. displays the worker ensemble
-    display_time_as_timestamp=False,  # unimportant; either way the program converts times to datetime object
+    display_time_as_timestamp=False,  # keep as False. Important for calculating the time and displaying it
     show_runtimes=False  # for displaying in the terminal how long each query and graph creation takes
     )
 
+# display graphs one at a time in a popup window. After closing one, the next one opens
 for graph_title, graph_df in graphs_dict.items():
     # skip empty graphs
     if graph_df is None:
         continue
 
     tick_spacing = 1
+    # show time in seconds since the initial time
     time_0 = graph_df["Time"][0]
     graph_df["Time"] = graph_df.apply(lambda row: row["Time"]-time_0, axis=1)
     my_datetime = dt.fromtimestamp(time_0)
-    print(my_datetime)
     s = my_datetime.strftime("%H:%M")
 
+    # graph styling and labels
     ax = sns.lineplot(data=graph_df, x="Time", y=graph_title, hue="Pod")
-    # ax.set(xlim=0)
-    # labels = ["13:44", "2023-08-30 13:50:42.138000128", "2023-08-30 13:55:42.138000128"]
-    # ax.set(xticklabels=labels)
     plt.title(graph_title)
     plt.xlabel("Seconds Since "+s)
     plt.xticks(rotation=25)
