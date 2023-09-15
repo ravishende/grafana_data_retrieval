1f31b844a9b6ff2df6ba850926de595fac5d8b6b
ravishende <ravi.sameer.shende@gmail.com>
2023-09-15 09:54:57 -0700

    added file for querying metrics found by node_metrics_retrieval and created new folder to keep them in

--- further_node_investigation/node_metrics_retrieval.py
+++ further_node_investigation/node_metrics_retrieval.py
@@ -1,9 +1,19 @@
+# For analyzing all potential information that can be queried regarding nodes.
+# Not necessary in the general program. More of a side enhancement.
+import sys
+import os
+# get set up to be able to import files from parent directory (grafana_data_retrieval)
+# utils.py and inputs.py not in this current directory and instead in the parent
+sys.path.append("../grafana_data_retrieval")
+current = os.path.dirname(os.path.realpath("header.py"))
+parent = os.path.dirname(current)
+sys.path.append(parent)
 from inputs import NAMESPACE
 from utils import write_json, query_api_site, get_result_list
 from pprint import pprint
 
-duration = '7h'
 # query for all metrics with node somewhere in the name
+duration = '1w'
 query = '{__name__ =~".*node.*", namespace="' + NAMESPACE + '"}[' + duration + ']'
 queried_data = query_api_site(query)
 
