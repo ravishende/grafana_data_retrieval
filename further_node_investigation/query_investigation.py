513bbfb3808925e20f9e0bdf46ce95f735a519dd
ravishende <ravi.sameer.shende@gmail.com>
2023-09-15 10:06:51 -0700

    added functionality of writing queried data to a file in query_investigation.py

--- further_node_investigation/query_investigation.py
+++ further_node_investigation/query_investigation.py
@@ -1,4 +1,4 @@
-# For getting information from queries retrieved by node_metrics_retrieval.py
+# This file is for getting information from queries retrieved by node_metrics_retrieval.py
 # To determine if this is information that should be saved or investigated further
 import sys
 import os
@@ -8,10 +8,11 @@ sys.path.append("../grafana_data_retrieval")
 current = os.path.dirname(os.path.realpath("header.py"))
 parent = os.path.dirname(current)
 sys.path.append(parent)
-from inputs import NAMESPACE
-from utils import read_json, query_api_site, get_result_list, print_sub_title, print_title
-from pprint import pprint
+from inputs import NAMESPACE, DEFAULT_DURATION
+from utils import read_json, write_json, query_api_site, get_result_list, print_sub_title, print_title
 from termcolor import colored
+from pprint import pprint
+
 
 duration = '15m'
 
@@ -24,11 +25,20 @@ for metric in metrics_list:
     query_str = f'{str(metric)}{{namespace="{NAMESPACE}"}}[{duration}]'
     queries_list.append(query_str)
 
-# get the data from the queries and print it.
-print_title("Queries Data")
+# store the data from the queries
+queried_data = {}
 for i in range(len(queries_list)):
     result_list = get_result_list(query_api_site(queries_list[i]))
-    print_sub_title(metrics_list[i])
+    metric_name = metrics_list[i]
+    queried_data[metric_name] = result_list
+
+#write queried data to a file
+write_json("queried_data.txt", queried_data)
+
+# print the queried data
+print_title("Queries Data")
+for metric_name, result_list in queried_data.items():
+    print_sub_title(metric_name)
     if len(result_list) == 0:
         print(colored("No data", "red"))
         continue
