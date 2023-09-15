a807b15743cf58121d3bff8869c06019063dd084
ravishende <ravi.sameer.shende@gmail.com>
2023-09-15 13:28:59 -0700

    changed graphs to display as datetimes in graph_visualization.py

--- graph_visualization.py
+++ graph_visualization.py
@@ -1,6 +1,8 @@
 import seaborn as sns
 import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
 from graphs import Graphs
+from termcolor import colored
 from datetime import datetime as dt
 
 # generate graphs dataframes
@@ -12,26 +14,25 @@ graphs_dict = graphs_class.get_graphs_dict(
     )
 
 # display graphs one at a time in a popup window. After closing one, the next one opens
+print("\nDisplaying graphs in popup window. To exit program, go to window and close all graphs.")
 for graph_title, graph_df in graphs_dict.items():
     # skip empty graphs
     if graph_df is None:
-        print("Graph has no data:", graph_title)
+        print("Graph has no data:", colored(graph_title, "red"))
         continue
 
-    tick_spacing = 1
-    # show time in seconds since the initial time
-    time_0 = graph_df["Time"][0]
-    graph_df["Time"] = graph_df.apply(lambda row: row["Time"]-time_0, axis=1)
-    my_datetime = dt.fromtimestamp(time_0)
-    s = my_datetime.strftime("%H:%M")
+    # show time as datetimes instead of seconds since epoch(01-01-1970)
+    graph_df["Time"] = graph_df["Time"].apply(dt.fromtimestamp)
 
     # graph styling and labels
+    tick_spacing = 1
     ax = sns.lineplot(data=graph_df, x="Time", y=graph_title, hue="Pod")
+    ax.xaxis.set_major_formatter(mdates.DateFormatter('%m/%d %H:%M'))
     sns.move_legend(ax, "upper left", bbox_to_anchor=(1, 1))
-    manager = plt.get_current_fig_manager()
-    manager.full_screen_toggle()
+    plt.get_current_fig_manager().full_screen_toggle()
     plt.subplots_adjust(right=0.75)  # right is default 0.9. right=0.75 moves the graph left so the legend is not partially cut off.
     plt.title(graph_title + " Over Time")
-    plt.xlabel("Seconds Since "+s)
+    # plt.xlabel("Seconds Since " + time_0_s)
+    plt.xlabel("Time")
     plt.xticks(rotation=25)
     plt.show()
