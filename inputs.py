f598228fc24ba6f6fbfe8d19936f02abbded0c30
ravishende <ravi.sameer.shende@gmail.com>
2023-10-16 11:59:14 -0700

    added input to inputs.py for specifying last data point time for graphs

--- inputs.py
+++ inputs.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
+from datetime import datetime
+
 
 # namespace
 NAMESPACE = 'wifire-quicfire'  # main namespace to be using
@@ -11,5 +13,6 @@ DEFAULT_DURATION = '10m'
 # graphs
 DEFAULT_GRAPH_STEP = '1m'
 DEFAULT_GRAPH_TIME_OFFSET = '1h'
+DEFAULT_FINAL_GRAPH_TIME = datetime.now() # must be a datetime object
 # requerying
 REQUERY_GRAPH_STEP_DIVISOR = 10  # a value of 10 means there will be 10x the number of datapoints for a given timeframe
