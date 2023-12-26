dc7cce77130e5097e5ecf97a4951e0206167c7f6
ravishende <ravi.sameer.shende@gmail.com>
2023-12-26 04:08:03 -0800

    added file for collecting successful runs from bp3d-runs.csv which contains only paths and statuses, not runs themselves

---
+++ training_data_handling/run_selection.py
@@ -0,0 +1,22 @@
+import pandas as pd
+
+
+'''
+This file is for selecting all successful runs from the original csv file that
+contains all runs with run_id, ensemble_uuid, run & ens status, and paths for the runs.
+You should not need to rerun it. 
+This original csv does not contain information on 
+input parameters of each run. 
+'''
+
+# settings and constants
+read_file = "csv_files/bp3d-runs.csv"
+write_file = "csv_files/successful_bp3d_runs.csv"
+
+runs = pd.read_csv(read_file)
+successful_runs = runs[(runs["ens_status"]=="Done") & (runs["run_status"]=="Done")]
+successful_runs = successful_runs.reset_index(drop=True)
+
+
+successful_runs.to_csv(write_file)
+print(successful_runs)
