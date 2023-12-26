47b2de8ad8773c229621bd47945ef906e68c9e51
ravishende <ravi.sameer.shende@gmail.com>
2023-12-26 04:10:15 -0800

    updated helpers folder

--- helpers/filtering.py
+++ helpers/filtering.py
@@ -1,21 +1,54 @@
+import re
+# gets the worker id for a given pod or returns None if it is not a bp3d-worker
+# def get_worker_id(pod_name):
+#     worker_title = 'bp3d-worker-'
+#     suffix = 'k8s-'
+    
+#     # if pod_name is a bp3d worker, return the worker id
+#     title_len = len(worker_title)
+#     suffix_len = len(suffix)
+#     if pod_name[0:title_len] == worker_title:
+#         # there are two types of bp3d worker pods. Some starting with 'bp3d-worker-', some with 'bp3d-worker-k8s-'.
+#         # collect the id for either type
+#         stripped_string = ""
+#         if pod_name[title_len:title_len+suffix_len] == suffix:
+#             stripped_string = pod_name[title_len+suffix_len:]
+#         else:
+#             stripped_string = pod_name[title_len:]
+
+#         # pods have following format: bp3d-worker-k8s-{UUID}-{jobIndex}-{k8s-id}
+#         # only select the UUID (first section after the prefix)
+#         ensemble_id =  stripped_string.split("-")[0]
+#         return ensemble_id
+    
+#     # if it isn't a worker pod, return None
+#     return None
+
+# for current bp3d-worker naming convention:
 # gets the worker id for a given pod or returns None if it is not a bp3d-worker
 def get_worker_id(pod_name):
-    worker_title = 'bp3d-worker-'
-    suffix = 'k8s-'
-    
-    # if pod_name is a bp3d worker, return the worker id
-    title_len = len(worker_title)
-    suffix_len = len(suffix)
-    if pod_name[0:title_len] == worker_title:
-        # there are two types of bp3d worker pods. Some starting with 'bp3d-worker-', some with 'bp3d-worker-k8s-'.
-        # collect the id for either type
-        if pod_name[title_len:title_len+suffix_len] == suffix:
-            return pod_name[title_len+suffix_len:-1]
-        else:
-            return pod_name[title_len:-1]
-    
-    # if it isn't a worker pod, return None
-    return None
+    regex_pattern = r"bp3d-worker-.*?-k8s-([a-fA-F0-9]+)"
+    match = re.search(regex_pattern, pod_name)
+    worker_id = match.group(1) if match else None
+    return worker_id
+
+# for old bp3d-worker naming convention:
+# gets the worker id for a given pod or returns None if it is not a bp3d-worker
+def get_worker_id_old_ptrn(pod_name):
+    old_regex_pattern = r"bp3d-worker-([a-fA-F0-9]+)-"
+    match = re.search(old_regex_pattern, pod_name)
+    worker_id = match.group(1) if match else None
+    return worker_id
+
+# gets the worker id for a given pod or returns None if it is not a bp3d-worker
+def get_worker_id_any_ptrn(pod_name):
+    old_id = get_worker_id_old_ptrn(pod_name)
+    if old_id is not None:
+        return old_id
+
+    current_id = get_worker_id(pod_name)
+    return current_id
+
 
 
 # for every worker pod in a given df, change pod's value to just be the worker id,
