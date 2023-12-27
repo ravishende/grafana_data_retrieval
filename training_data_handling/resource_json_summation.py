38dce53855c929b007da9a485aa3585f9a2ce61a
ravishende <ravi.sameer.shende@gmail.com>
2023-12-27 14:23:08 -0800

    got resource_json_summation.py working

--- training_data_handling/resource_json_summation.py
+++ training_data_handling/resource_json_summation.py
@@ -22,16 +22,18 @@ that contains the ensemble that each run is a part of.
 # settings
 pd.set_option("display.max_columns", None)
 pd.set_option("display.max_rows", None)
-read_file = "csv_files/" #p2_ensemble_included.csv
-write_file = "csv_files/" #calculated_training_data.csv
+read_file = "csv_files/queried_w_ids.csv" #p2_ensemble_included.csv
+write_file = "csv_files/summed.csv" #calculated_training_data.csv
 
 
 # given a json result_list return a float summation over all pods' values in that ensemble
 def sum_pods_for_ensemble(result_list, ensemble):
-    # get str as json
-    total = 0
+    # handle if there is no ensemble id
     if not ensemble:
         return result_list
+
+    # get str as json
+    total = 0
 
     # loop over pods in ensemble, adding values to sum
     for item in result_list:
@@ -51,7 +53,7 @@ def sum_pods_for_ensemble(result_list, ensemble):
 # in every row where there was an ensemble id in the ensemble column
 def update_col(df, update_col_title, ensemble_col_title):
     # change result_list strings to python lists
-    # df[update_col_title] = df[update_col_title].apply(literal_eval)
+    df[update_col_title] = df[update_col_title].apply(literal_eval)
 
     # calculate totals summed over the ensemble for given column
     df[update_col_title] = df.apply(
@@ -61,34 +63,43 @@ def update_col(df, update_col_title, ensemble_col_title):
     return df[update_col_title]
 
 
-def get_first_30(pod_name):
-    if len(pod_name) >= 30:
-        return pod_name[:30]
-    return pod_name
+# def get_first_30(pod_name):
+#     if len(pod_name) >= 30:
+#         return pod_name[:30]
+#     return pod_name
 
-def get_ids(res_list):
-    ids = []
-    for item in res_list:
-        ids.append(get_first_30(item['metric']['pod']))
-    return ids
+# def get_ids(res_list):
+#     ids = []
+#     for item in res_list:
+#         ids.append(get_worker_id(item['metric']['pod']))
+#     return ids
 
-# get the csv file as a pandas dataframe
-json_training_data = pd.read_csv(read_file, index_col=0)
 
 # select columns to update (sum over)
-ensemble_col = "ensemble"
-cpu_tot_col = "cpu_usage"
-mem_tot_col = "mem_usage"
-cpu_t1_col = "cpu_t1"
-mem_t1_col = "mem_t1"
-cpu_t2_col = "cpu_t2"
-mem_t2_col = "mem_t2"
+ensemble_col = "ensemble_uuid"
+cpu_tot_col = "cpu_total"
+mem_tot_col = "mem_total"
+# cpu_t1_col = "cpu_t1"
+# mem_t1_col = "mem_t1"
+# cpu_t2_col = "cpu_t2"
+# mem_t2_col = "mem_t2"
 
-calculated_training_data = json_training_data.dropna(subset=ensemble_col)
-calculated_training_data[cpu_tot_col] = calculated_training_data[cpu_tot_col].apply(literal_eval)
+# get the csv file as a pandas dataframe
+summed_runs = pd.read_csv(read_file, index_col=0)
+summed_runs[cpu_tot_col] = update_col(summed_runs, cpu_tot_col, ensemble_col)
+summed_runs[mem_tot_col] = update_col(summed_runs, mem_tot_col, ensemble_col)
+
+summed_runs.to_csv(write_file)
+# summed_runs[mem_tot_col] = summed_runs[mem_tot_col].apply(literal_eval)
+# summed_runs["mem_worker_ids"] = summed_runs[mem_tot_col].apply(get_ids)
+
+
+
+# calculated_training_data = json_training_data.dropna(subset=ensemble_col)
+# calculated_training_data[cpu_tot_col] = calculated_training_data[cpu_tot_col].apply(literal_eval)
 # update columns - sum json to get single float
-calculated_training_data["worker_ids"] = calculated_training_data[cpu_tot_col].apply(get_ids)
-calculated_training_data[cpu_tot_col] = update_col(calculated_training_data, cpu_tot_col, ensemble_col)
+# calculated_training_data["worker_ids"] = calculated_training_data[cpu_tot_col].apply(get_ids)
+# # calculated_training_data[cpu_tot_col] = update_col(calculated_training_data, cpu_tot_col, ensemble_col)
 # calculated_training_data[mem_tot_col] = update_col(calculated_training_data, mem_tot_col, ensemble_col)
 # calculated_training_data[cpu_t1_col] = update_col(calculated_training_data, cpu_t1_col, ensemble_col)
 # calculated_training_data[mem_t1_col] = update_col(calculated_training_data, mem_t1_col, ensemble_col)
@@ -97,10 +108,12 @@ calculated_training_data[cpu_tot_col] = update_col(calculated_training_data, cpu
 
 # print df and write to a new file
 # print("na ensembles", calculated_training_data[ensemble_col].isna().sum())
-print(calculated_training_data[cpu_tot_col].head(1000))
-print("\n"*5)
-print(calculated_training_data["worker_ids"].head(1000))
-
+subset = summed_runs[[cpu_tot_col, mem_tot_col, 'runtime']]
+print(subset.head(1000))
+# print(summed_runs[mem_tot_col].head(500))
+# print("\n"*5)
+# print(summed_runs["mem_worker_ids"].head(500))
+# print("\n"*5)
 # calculated_training_data.to_csv(write_file)
 
 
