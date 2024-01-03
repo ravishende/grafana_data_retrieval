f95f490c7d77e1f4d02d2d2234b4215133cb9fa8
ravishende <ravi.sameer.shende@gmail.com>
2024-01-02 16:53:06 -0800

    updated finalize_training_data and training_data.csv

--- training_data_handling/finalize_training_data.py
+++ training_data_handling/finalize_training_data.py
@@ -1,34 +1,36 @@
 import pandas as pd
 import shutil
 
-# display settings
-pd.set_option("display.max_columns", None)
-terminal_width = shutil.get_terminal_size().columns
-pd.set_option('display.width', terminal_width)
-
 '''
 file to get rid of all unnecessary columns for training a model
 and dressing up anything else to help
 (like changing cpu_t1 to be an average collected up until t1)
 '''
 
-read_file = 'csv_files/non_zeros.csv'
+# settings
+read_file = 'csv_files/ratios_added.csv'
 write_file = 'csv_files/training_data.csv'
+
+# display settings
+pd.set_option("display.max_columns", None)
+terminal_width = shutil.get_terminal_size().columns
+pd.set_option('display.width', terminal_width)
 
 
 useless_columns = [
     'path',
     'start',
-    'stop',
-    'timestep',  # maybe? - needs more analysis
-    'ensemble_uuid',
-    'run_uuid'
+    'stop', 
+    'timestep',  # no variations in values - all 600
+    'ensemble_uuid', 
+    'run_uuid' 
 ]
 
 # read csv and drop unnecessary columns
-summed = pd.read_csv(read_file, index_col=0)
-summed = summed.drop(columns=useless_columns)
+whole_df = pd.read_csv(read_file, index_col=0)
+training_data = whole_df.drop(columns=useless_columns)
 
 # write updated df to csv file and print it
-summed.to_csv(write_file)
-print(summed)
+training_data.to_csv(write_file)
+print("\n"*3)
+print(training_data, "\n"*3)
