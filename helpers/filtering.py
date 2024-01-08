9b74fa641633675306b55ab9cb4e24d957c9187c
ravishende <ravi.sameer.shende@gmail.com>
2024-01-08 11:00:59 -0800

    minor file formatting in helpers/filtering.py

--- helpers/filtering.py
+++ helpers/filtering.py
@@ -1,6 +1,6 @@
 import re
 
-# for current bp3d-worker naming convention:
+# for current bp3d-worker naming convention ((bp3d-worker-k8s-...):
 # gets the worker id for a given pod or returns None if it is not a bp3d-worker
 def get_worker_id(pod_name):
     regex_pattern = r"bp3d-worker-k8s-([a-fA-F0-9]+)"
@@ -8,7 +8,20 @@ def get_worker_id(pod_name):
     worker_id = match.group(1) if match else None
     return worker_id
 
-# for old bp3d-worker naming convention:
+
+# for every worker pod in a given df, change pod's value to just be the worker id,
+# drop all non-worker pods, then return that new, filtered dataframe
+def filter_df_for_workers(dataframe):
+    # run get_worker_id() on all pods to replace the pod with the ensemble or None if not a worker
+    dataframe['Pod'] = dataframe['Pod'].apply(get_worker_id)
+    # drop all the rows with non worker pods
+    dataframe = dataframe.dropna(subset=["Pod"])
+    return dataframe
+
+
+# potentially useful functions:
+
+# for old bp3d-worker naming convention (bp3d-worker-...):
 # gets the worker id for a given pod or returns None if it is not a bp3d-worker
 def get_worker_id_old_ptrn(pod_name):
     old_regex_pattern = r"bp3d-worker-([a-fA-F0-9]+)-"
@@ -24,19 +37,6 @@ def get_worker_id_any_ptrn(pod_name):
 
     current_id = get_worker_id(pod_name)
     return current_id
-
-
-
-# for every worker pod in a given df, change pod's value to just be the worker id,
-# drop all non-worker pods, then return that new, filtered dataframe
-def filter_df_for_workers(dataframe):
-    # run get_worker_id() on all pods to replace the pod with the ensemble or None if not a worker
-    dataframe['Pod'] = dataframe['Pod'].apply(get_worker_id)
-    # drop all the rows with non worker pods
-    dataframe = dataframe.dropna(subset=["Pod"])
-    return dataframe
-
-
 
 # gets the worker id for a given pod or returns None if it is not a bp3d-worker
 # def get_worker_id(pod_name):
