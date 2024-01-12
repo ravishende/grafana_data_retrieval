2178e14651128339de83c8b6da596c1914b97896
ravishende <ravi.sameer.shende@gmail.com>
2024-01-12 10:46:43 -0800

    updated helpers printing functions to center text within the printed outline borders

--- helpers/printing.py
+++ helpers/printing.py
@@ -1,23 +1,34 @@
 from termcolor import colored
 
+# add enough padding to a string to center it within its border
+def pad_str(string, border_len):
+    padding_len = (border_len - len(string)) // 2
+    padded_str = " "*padding_len + string
+    return padded_str
+
 # Printing Functions
 def print_heading(heading):
-    indented_heading = " "*15 + heading + ":"
-    print("\n\n\n\n" + "*"*100)
-    print(colored(indented_heading, "magenta"))
-    print("*" * 100)
+    border_len = 100
+    padded_heading = pad_str(heading, border_len)
+    print("\n\n\n\n" + "*"*border_len)
+    print(colored(padded_heading, "magenta"))
+    print("*" * border_len)
 
 
 def print_title(title):
-    print("\n\n" + "-"*100, "\n")
-    print("            ", colored(title, "green"))
-    print("-" * 100, "\n")
+    border_len = 75
+    padded_title = pad_str(title, border_len)
+    print("\n\n" + "-" * border_len)
+    print(colored(padded_title, "green"))
+    print("-" * border_len, "\n")
 
 
 def print_sub_title(sub_title):
-    print("\n\n" + '='*30)
-    print(colored(sub_title, "blue"))
-    print('='*30)
+    border_len = 30
+    paded_subtitle = pad_str(sub_title, border_len)
+    print("\n\n" + '=' * border_len)
+    print(colored(paded_subtitle, "blue"))
+    print('=' * border_len)
 
 
 # for a given dictionary in the form {titles:dataframes}
