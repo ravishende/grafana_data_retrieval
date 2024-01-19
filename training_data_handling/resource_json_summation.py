889f933925d76519afcf3e1e3cf316b9a8353352
ravishende <ravi.sameer.shende@gmail.com>
2024-01-19 15:45:20 -0800

    updated resource_json_summation to work for all metrics queried in updated query_resources.py

--- training_data_handling/resource_json_summation.py
+++ training_data_handling/resource_json_summation.py
@@ -28,8 +28,10 @@ terminal_width = shutil.get_terminal_size().columns
 pd.set_option('display.width', terminal_width)
 # pd.set_option("display.max_rows", None)
 
-read_file = "csv_files/nonzero_queried_all_metrics.csv" 
-write_file = "csv_files/summed_all_metrics.csv"
+read_file = "csv_files/queried.csv" 
+# read_file = "csv_files/updated_requests.csv" 
+write_file = "csv_files/summed.csv"
+# write_file = "csv_files/updated_requests.csv"
 # success_write_file = "csv_files/summed_success.csv"
 # na_write_file = "csv_files/summed_na.csv"
 
@@ -85,8 +87,10 @@ def sum_pods_for_ensemble(result_list, ensemble, static=False):
 # in every row where there was an ensemble id in the ensemble column, or None if there are
 # no bp3d-worker-pods
 def update_col(df, update_col_title, ensemble_col_title, static=False):
+    # drop na values of column
     df = df.dropna(subset=update_col_title)
-    # change result_list strings to python lists
+
+    # get result_list as list (not string)
     df[update_col_title] = df[update_col_title].apply(literal_eval)
 
     # calculate totals summed over the ensemble for given column
@@ -99,9 +103,9 @@ def update_col(df, update_col_title, ensemble_col_title, static=False):
 # given a dataframe, list of column names to update, and title of the ensemble_id column,
 # return a new dataframe with the edited columns being float values of the summed over json data
 def update_columns(df, update_col_names, ensemble_col_title, static=False):
-    len_cols_to_update = len(update_col_names)
+    num_update_cols = len(update_col_names)
     for i, col_name in enumerate(update_col_names):
-        print(f"\n\nUpdating {col_name}", colored(f"({i+1}/{len_cols_to_update})", "green"))
+        print(f"\n\nUpdating {col_name}", colored(f"({i+1}/{num_update_cols})", "green"))
         df = update_col(df, col_name, ensemble_col_title, static=static)
     return df
 
@@ -113,82 +117,68 @@ def update_columns(df, update_col_names, ensemble_col_title, static=False):
 ============================================
 '''
 
-# rename cpu and mem columns for consistent naming conventions
-rename_cols_dict = {
-    "cpu_total":"cpu_usage_total",
-    "cpu_t1":"cpu_usage_t1",
-    "cpu_t2":"cpu_usage_t2",
-    "mem_total":"mem_usage_total",
-    "mem_t1":"mem_usage_t1",
-    "mem_t2":"mem_usage_t2",
-}
 
 # all queried metrics
-metrics = [
-    # "cpu_usage",
-    # "mem_usage",
-    # "cpu_request",
-    # "mem_request",
+all_metrics = [
+    "cpu_usage",
+    "mem_usage",
+    "cpu_request",
+    "mem_request",
     "transmitted_packets",
     "received_packets",
     "transmitted_bandwidth",
     "received_bandwidth"
 ]
 
-# get all columns to sum
-columns_to_sum = get_columns_from_metrics(metrics)
-
-# total static columns (the static columns that have been queried)
-static_columns_total = [
-    "cpu_request_total",
-    "mem_request_total",
+static_metrics = [
+    "cpu_request",
+    "mem_request",
 ]
+
+metrics_to_sum = [metric for metric in all_metrics if metric not in static_metrics]
+# get all columns to sum
+columns_to_sum = get_columns_from_metrics(metrics_to_sum)
 
 # select columns to update (sum over) as well as ensemble ids column
 ensemble_col = "ensemble_uuid"
 
-# get the csv file as a pandas dataframe, and fix naming conventions
+# get the csv file as a pandas dataframe
 summed_runs = pd.read_csv(read_file, index_col=0)
-summed_runs = summed_runs.rename(columns=rename_cols_dict)
-
-# add in non queried static runs
-static_columns_t1 = [name.replace("total", "t1") for name in static_columns_total]
-static_columns_t2 = [name.replace("total", "t2") for name in static_columns_total]
-for i, col_name in enumerate(static_columns_total):
-    summed_runs[static_columns_t1[i]] = summed_runs[col_name]
-    summed_runs[static_columns_t2[i]] = summed_runs[col_name]
-all_static_columns = static_columns_total + static_columns_t1 + static_columns_t2
 
 # update columns to get float values from json
-print_heading("Summing Up Normal Columns")
+print_heading("Summing Up Columns")
 summed_runs = update_columns(summed_runs, columns_to_sum, ensemble_col, static=False)
 print_heading("Getting Floats for Static Columns")
-summed_runs = update_columns(summed_runs, all_static_columns, ensemble_col, static=True)
+summed_runs = update_columns(summed_runs, static_metrics, ensemble_col, static=True)
 
 # get percentage metrics in a format of [%_col, numerator_col, denominator_col]
 percentage_column_formats = [
-    ["cpu_request_%_total", "cpu_usage_total", "cpu_request_total"],
-    ["cpu_request_%_t1", "cpu_usage_t1", "cpu_request_t1"],
-    ["cpu_request_%_t2", "cpu_usage_t2", "cpu_request_t2"],
-    ["mem_request_%_total", "mem_usage_total", "mem_request_total"],
-    ["mem_request_%_t1", "mem_usage_t1", "mem_request_t1"],
-    ["mem_request_%_t2", "mem_usage_t2", "mem_request_t2"],
+    ["cpu_request_%_total", "cpu_usage_total", "cpu_request"],
+    ["cpu_request_%_t1", "cpu_usage_t1", "cpu_request"],
+    ["cpu_request_%_t2", "cpu_usage_t2", "cpu_request"],
+    ["mem_request_%_total", "mem_usage_total", "mem_request"],
+    ["mem_request_%_t1", "mem_usage_t1", "mem_request"],
+    ["mem_request_%_t2", "mem_usage_t2", "mem_request"],
 ]
 # calculate percentage columns
 for metric_list in percentage_column_formats:
     summed_runs[metric_list[0]] = 100 * summed_runs[metric_list[1]] / summed_runs[metric_list[2]]
 
+summed_runs.to_csv(write_file)
+print(summed_runs)
+
+
 '''
 # split the summed_runs into runs that had data for total resources and for ones that didn't (no bp3d-workers)
 # in other words, if cpu_tot or mem_tot are none, add row to na_mask
-na_mask = summed_runs[cpu_tot].isna() | summed_runs[mem_tot].isna()
-na_worker_runs = summed_runs[na_mask]
-valid_worker_runs = summed_runs[~na_mask]
-'''
+# na_mask = summed_runs[cpu_tot].isna() | summed_runs[mem_tot].isna()
+# na_worker_runs = summed_runs[na_mask]
+# valid_worker_runs = summed_runs[~na_mask]
 
+# summed_runs = update_col(summed_runs, "cpu_request_total", ensemble_col, static=True)
+# summed_runs = update_col(summed_runs, "mem_request_total", ensemble_col, static=True)
 # save dataframes to new files and print summed_runs
-summed_runs.to_csv(write_file)
-print(summed_runs)
+
 # valid_worker_runs.to_csv(success_write_file)
 # na_worker_runs.to_csv(na_write_file)
 
@@ -200,7 +190,7 @@ print(summed_runs)
 #     return ids
 
 
-# update columns (sum json-like data to get single float)
+# update column (sum json-like data to get single float) for multiple columns
 # cpu
 # summed_runs = update_col(summed_runs, cpu_tot, ensemble_col)
 # summed_runs = update_col(summed_runs, cpu_t1, ensemble_col)
@@ -209,3 +199,4 @@ print(summed_runs)
 # summed_runs = update_col(summed_runs, mem_tot, ensemble_col)
 # summed_runs = update_col(summed_runs, mem_t1, ensemble_col)
 # summed_runs = update_col(summed_runs, mem_t2, ensemble_col)
+'''
