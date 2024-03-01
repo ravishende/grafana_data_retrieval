b28ea2adaf8db414d5eca17464049623d5f3c8fd
ravishende <ravi.sameer.shende@gmail.com>
2024-03-01 13:47:59 -0800

    made phase_3 into a class

--- training_data_handling/automate_training_data_collection/phase_3.py
+++ training_data_handling/automate_training_data_collection/phase_3.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import shutil
-from workflow_files import phase3_files
 from itertools import chain
+from workflow_files import PHASE_3_FILES
 import sys
 import os
 sys.path.append("../../grafana_data_retrieval")  # Adjust the path to go up two levels
@@ -21,115 +21,96 @@ pd.set_option('display.width', terminal_width)
     Helper functions
 ==========================
 '''
+class Phase_1():
+    def __init__(self, files=PHASE_3_FILES):
+        self.files = files
+        # metrics (to be queried)
+        self.all_metrics = [
+            "cpu_usage",
+            "mem_usage",
+            "cpu_request",
+            "mem_request",
+            "transmitted_packets",
+            "received_packets",
+            "transmitted_bandwidth",
+            "received_bandwidth"
+            ]
+        self.static_metrics = ["cpu_request", "mem_request"]
+        self.non_static_metrics = [metric for metric in self.all_metrics if metric not in self.static_metrics]
+        # duration columns (for querying)
+            # Note: assumes duration columns are in the form "duration_t{N}" where {N} is 
+            # an int from 1 to num_duration_cols inclusive
+        self.num_duration_cols = self._init_num_duration_cols()
+        self.duration_col_names = ["duration_t" + str(num) for num in range(1, self.num_duration_cols+1)]
+        self.duration_col_total = "runtime"  # from phase_2
+        # column names (for queried results)
+        self.col_names_static = self.static_metrics
+        self.col_names_total = [name + "_total" for name in self.non_static_metrics]
+        self.col_names_by_time = self._init_col_names_by_time()
+        self.all_col_names = self.col_names_static + self.col_names_total + list(chain.from_iterable(self.col_names_by_time))
 
-NUM_DURATION_COLS = None
+    # read a txt file written by phase_2 that contains num_duration_cols
+    def _init_num_duration_cols(self):
+        # read the file to get num_duration_cols
+        try:
+            with open(self.files['num_duration_cols'],"r") as f:
+                num_duration_cols = int(f.read())
+        # handle errors for reading file
+        except FileNotFoundError:
+            print(f"Error: File {self.files['num_duration_cols']} not found.")
+        except ValueError:
+            print(f"Error: Content of {self.files['num_duration_cols']} is not an integer.")
+        except Exception as e:
+            print(f"An unexpected error occurred: {e}")
+        # return if successful
+        return num_duration_cols
 
-def _get_num_duration_cols():
-    global NUM_DURATION_COLS
-    if NUM_DURATION_COLS is not None:
-        return NUM_DURATION_COLS
+    # initialize col_names_by_time based on self.num_duration_cols and self.non_static_metrics
+    def _init_col_names_by_time(self):
+        col_names_by_time = []
+        for i in range(1, self.num_duration_cols):
+            col_names_t_i = [name + "_t" + str(i) for name in self.non_static_metrics]
+            col_names_by_time.append(col_names_t_i)
+        return col_names_by_time
 
-    with open("num_duration_cols.txt","r") as f:
-        num_duration_cols = int(f.read())
-    
-    NUM_DURATION_COLS = num_duration_cols
-    return num_duration_cols
+    # given: 
+        # df - a dataframe 
+        # batch_size - number of rows to query at a time until the df is filled out
+    # query all important metrics, saving to the temporary_save_file after inserting columns of the same duration column.
+    # return the updated dataframe with all columns queried
+    def query_metrics(self, df, batch_size):
+        # initialize all queried column names in the dataframe if they aren't already
+        for col_name in self.all_col_names:
+            if col_name not in df.columns:
+                df[col_name] = None
 
+        # while there are still unqueried rows, keep querying batch_size rows at a time
+        while df[self.col_names_total[0]].iloc[len(df)-1] is None:
+            # query and insert static and total columns
+            df = query_and_insert_columns(df, self.static_metrics, self.col_names_static, self.duration_col_total, batch_size)
+            df.to_csv(self.files['temp'])
+            df = query_and_insert_columns(df, self.non_static_metrics, self.col_names_total, self.duration_col_total, batch_size)
+            df.to_csv(self.files['temp'])
+            # query and insert duration_t_i columns
+            for i, col_names_t_i in enumerate(self.col_names_by_time):
+                df = query_and_insert_columns(df, self.non_static_metrics, col_names_t_i, self.duration_col_names[i], batch_size)
+                df.to_csv(self.files['temp'])
 
+        return df
 
-# get the static and non_static metrics lists
-def _get_metrics():
-    # define the metrics to be queried
-    # list of all metrics you can query (with query_and_insert_columns())
-    all_metrics = [
-        "cpu_usage",
-        "mem_usage",
-        "cpu_request",
-        "mem_request",
-        "transmitted_packets",
-        "received_packets",
-        "transmitted_bandwidth",
-        "received_bandwidth"
-        ]
-    # metrics that don't change over a run
-    static_metrics = ["cpu_request", "mem_request"]
-    # metrics that do change over a run
-    non_static_metrics = [metric for metric in all_metrics if metric not in static_metrics]
+    '''
+    ======================
+        Main Program
+    ======================
+    '''
+    def run(self):
+        # get preprocessed_df
+        preprocessed_df = pd.read_csv(phase3_files['read'], nrows=3)
 
-    return static_metrics, non_static_metrics
+        # 7. query resource metrics (metrics total, t1, t2)
+        rows_batch_size = 20
+        queried_df = self.query_metrics(preprocessed_df, rows_batch_size, temporary_save_file)
 
-
-def _get_metric_column_names():
-    static_metrics, non_static_metrics = _get_metrics()
-    # get column names
-    col_names_static = static_metrics
-    col_names_total = [name + "_total" for name in non_static_metrics]
-    num_duration_cols = _get_num_duration_cols()
-
-    # get col_names_t1, col_names_t2, etc. in a list called col_names_by_time
-    col_names_by_time = []
-    for i in range(1, num_duration_cols):
-        col_names_t_i = [name + "_t" + str(i) for name in non_static_metrics]
-        col_names_by_time.append(col_names_t_i)
-    return col_names_static, col_names_total, col_names_by_time
-
-# given: 
-    # df - a dataframe 
-    # batch_size - number of rows to query at a time until the df is filled out
-    # temporary_save_file - name of a csv file to save df to after each big insert in case the program is stopped
-# query all important metrics, saving to the temporary_save_file after inserting columns of the same duration column.
-    # Note: this function assumes the total duration column is "runtime" and duration columns 
-    # are in the form "duration_t{N}" where {N} is an int from 1 to num_duration_cols inclusive
-# return the updated dataframe with all columns queried
-def query_metrics(df, batch_size, temporary_save_file):
-    # get metrics lists and number of duration columns
-    static_metrics, non_static_metrics = _get_metrics()
-    num_duration_cols = _get_num_duration_cols()
-
-    # get duration column names
-    duration_col_names = ["duration_t" + str(num) for num in range(1,num_duration_cols+1)]
-    duration_col_total = "runtime"
-
-    # get metric column names
-    col_names_static, col_names_total, col_names_by_time = _get_metric_column_names()
-
-    # collect all metric column names and initialize them in the dataframe if they aren't already
-    all_col_names = col_names_static + col_names_total + list(chain.from_iterable(col_names_by_time))
-
-    for col_name in all_col_names:
-        if col_name not in df.columns:
-            df[col_name] = None
-
-    # while there are still unqueried rows, keep querying batch_size rows at a time
-    while df[col_names_total[0]].iloc[len(df)-1] is None:
-        # query and insert static and total columns
-        df = query_and_insert_columns(df, static_metrics, col_names_static, duration_col_total, batch_size)
-        df.to_csv(temporary_save_file)
-        df = query_and_insert_columns(df, non_static_metrics, col_names_total, duration_col_total, batch_size)
-        df.to_csv(temporary_save_file)
-        # query and insert duration_t_i columns
-        for i, col_names_t_i in enumerate(col_names_by_time):
-            df = query_and_insert_columns(df, non_static_metrics, col_names_t_i, duration_col_names[i], batch_size)
-            df.to_csv(temporary_save_file)
-
-    return df
-
-
-
-'''
-======================
-    Main Program
-======================
-'''
-
-# get preprocessed_df
-preprocessed_df = pd.read_csv(phase3_files['read'], nrows=3)
-
-# 7. query resource metrics (metrics total, t1, t2)
-temporary_save_file = "csvs/query_progress.csv"
-rows_batch_size = 20
-queried_df = query_metrics(preprocessed_df, rows_batch_size, temporary_save_file)
-
-# save df to a csv file
-queried_df.to_csv(phase3_files['write'])
-print(queried_df)
+        # save df to a csv file
+        queried_df.to_csv(phase3_files['write'])
+        print(queried_df)
