271ede72ae6f7b34e97b520403936b9eb0d6fa91
ravishende <ravi.sameer.shende@gmail.com>
2024-03-18 03:32:07 -0700

    added compute_resources_dashboard.py in dashboards/

---
+++ dashboards/compute_resources_dashboard.py
@@ -0,0 +1,41 @@
+import sys
+import os
+sys.path.append("../grafana_data_retrieval")  # Adjust the path to go up one level
+current = os.path.dirname(os.path.realpath(__file__))
+parent = os.path.dirname(current)
+sys.path.append(parent)
+from graphs import Graphs
+from graph_visualization import display_graphs
+from helpers.printing import print_dataframe_dict
+
+# display settings
+get_graphs_as_single_df=False
+visualize_graphs=False
+
+host = "node-2-3.sdsc.optiputer.net"
+queries = {
+    'GPU Utilization':'DCGM_FI_DEV_GPU_UTIL * on (namespace, pod) group_left(node) node_namespace_pod:kube_pod_info:{node="' + host + '"}',
+    'Memory Copy Utilization':'DCGM_FI_DEV_MEM_COPY_UTIL * on (namespace, pod) group_left(node) node_namespace_pod:kube_pod_info:{node="' + host + '"}',
+    'Power':'DCGM_FI_DEV_POWER_USAGE * on (namespace, pod) group_left(node) node_namespace_pod:kube_pod_info:{node="' + host + '"}',
+    'Temperature':'DCGM_FI_DEV_GPU_TEMP * on (namespace, pod) group_left(node) node_namespace_pod:kube_pod_info:{node="' + host + '"}',
+    'Fan Speed':'ipmi_fan_speed * on (namespace, pod) group_left(node) node_namespace_pod:kube_pod_info:{node="' + host + '"}'
+}
+
+# get graphs class
+graphs_class = Graphs()
+# get dict containing all queried graphs
+graphs_dict = graphs_class.get_graphs_from_queries(queries, sum_by=["namespace", "pod"])
+
+
+# logic for displaying graphs
+if not get_graphs_as_single_df:
+    # print multiple graphs
+    print_dataframe_dict(graphs_dict)
+else:
+    # print graphs as single dataframe
+    graphs_df = graphs_class.get_graphs_as_one_df(graphs_dict)
+    print(graphs_df)
+
+if visualize_graphs:
+    # display graphs in another window 
+    display_graphs(graphs_dict, sum_by="instance")
