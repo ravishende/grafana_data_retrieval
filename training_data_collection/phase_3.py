aed12452693e67b243d69e68e76fbeb709a4e43a
ravishende <ravi.sameer.shende@gmail.com>
2024-04-13 21:28:29 -0700

    fixed bugs and added functionality to reuse saved query_progress df in phase_3.py

--- training_data_collection/phase_3.py
+++ training_data_collection/phase_3.py
@@ -32,23 +32,34 @@ class Phase_3():
     # query all important metrics, saving to the temporary_save_file after inserting columns of the same duration column.
     # return the updated dataframe with all columns queried
     def query_metrics(self, df, batch_size):
+        # try getting saved query progress df
+        try:
+            # if the first cell of the last queried column in progress_df is not NA, use progress_df instead of df
+            progress_df = pd.read_csv(self.files['query_progress'], index_col=0)
+            progress_df_final_col = progress_df[self.col_names_total[-1]]
+            first_cell = progress_df_final_col.iloc[0]
+            if not pd.isna(first_cell):
+                df = progress_df
+        except:
+            pass
+
         # initialize all queried column names in the dataframe if they aren't already
         for col_name in self.all_col_names:
             if col_name not in df.columns:
                 df[col_name] = None
 
         # while there are still unqueried rows, keep querying batch_size rows at a time
-        while df[self.col_names_total[0]].iloc[len(df)-1] is None:
+        while df[self.col_names_total[-1]].iloc[len(df)-1] is None:
             # query and insert static columns
             df = query_and_insert_columns(df, self.static_metrics, self.col_names_static, self.duration_col_total, batch_size)
-            # df.to_csv(self.files['query_progress'])
-            # query and insert total columns
-            df = query_and_insert_columns(df, self.non_static_metrics, self.col_names_total, self.duration_col_total, batch_size)
             df.to_csv(self.files['query_progress'])
             # query and insert duration_t_i columns
             for i, col_names_t_i in enumerate(self.col_names_by_time):
                 df = query_and_insert_columns(df, self.non_static_metrics, col_names_t_i, self.duration_col_names[i], batch_size)
                 df.to_csv(self.files['query_progress'])
+            # query and insert total columns
+            df = query_and_insert_columns(df, self.non_static_metrics, self.col_names_total, self.duration_col_total, batch_size)
+            df.to_csv(self.files['query_progress'])
 
         return df
 
@@ -62,7 +73,7 @@ class Phase_3():
         success = False
 
         # get preprocessed_df
-        preprocessed_df = pd.read_csv(self.files['read'], nrows=3)
+        preprocessed_df = pd.read_csv(self.files['read'], index_col=0)
 
         # 7. query resource metrics (metrics total, t1, t2)
         queried_df = self.query_metrics(preprocessed_df, rows_batch_size)
