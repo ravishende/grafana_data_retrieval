57b16acfbdc55d5c9cbb861ea64ec26925aa52d4
ravishende <ravi.sameer.shende@gmail.com>
2024-07-14 03:11:12 -0700

    minor code tweaks and updated comments in work_flow.py

--- training_data_collection/work_flow.py
+++ training_data_collection/work_flow.py
@@ -5,10 +5,12 @@ from phase_1 import Phase_1
 from phase_2 import Phase_2
 from phase_3 import Phase_3
 from phase_4 import Phase_4
-from work_flow_functions import is_phase_finished, set_phase_finished, initialize_files, reset_phases_progress, prompt_set_num_duration_cols
+from training_data_collection.work_flow_functions import prompt_helitack_status
+from work_flow_functions import is_phase_finished, set_phase_finished, display_new_run_prompt, prompt_helitack_status
 
 # display settings
 pd.set_option("display.max_columns", None)
+pd.set_option("display.max_rows", 100)
 terminal_width = shutil.get_terminal_size().columns
 pd.set_option('display.width', terminal_width)
 
@@ -17,14 +19,26 @@ NOTE: If this is the first time ever running this file:
 all txt and csv files with their paths will be setup for you EXCEPT:
 you will need to put in the PHASE_1_FILES['read'] file with the appropiate name into csvs/
 Be sure to check workflow_files.py for the proper name of this file, but it is most likely
-'phase_1_read.csv'. 
+'phase_1_read.csv' (in the csvs folder). 
 It should be a DataFrame containing the following columns:
 run_uuid, ensemble_uuid, <-- mandatory
-run_status, ens_status   <-- optional
+queue_time, run_status, ens_status   <-- optional
 
 Finally, make sure that new_run is set to True at the start of the Main Program below
 '''
 
+'''
+========================
+    User Settings
+========================
+'''
+
+# if this is the first time running it with new data, set new_run to True. Otherwise, if you are partway through running it, set new_run to False.
+new_run = True
+# if running into unexpected issues in phase 1 with paths and run uuids not matching up, try setting thorough_refresh to True. Otherwise, keep it False to not waste time regathering paths.
+thorough_refresh = False
+# if we're looking at helitack (nonstandard - dev) runs, set this to True, otherwise by default, set it to False
+is_helitack_run = True
 
 '''
 ========================
@@ -32,30 +46,17 @@ Finally, make sure that new_run is set to True at the start of the Main Program 
 ========================
 '''
 
-# if this is the first time running it with new data, set new_run to True. Otherwise, if you are partway through running it, set new_run to False.
-new_run = True
-if new_run:
-    # ask user if they meant to start a new run or continue an old one
-    new_run_message = "\
-        \nATTENTION: new_run is set to True. This means that all phases' progress will be reset.\
-        \nEverything except paths in phase_1 will have to be regathered.\
-        \nAre you sure you want to continue?"
-    print(colored(new_run_message, "red"))
-    response = input("Type 'y' to continue resetting the progress. Any other response will continue as if new_run were set to False.\n")
-    # if it is a new run, initialize files and reset progress, wiping files (but not paths gathered)
-    if response == "y":
-        initialize_files()
-        reset_phases_progress(wipe_files=True)
-        # initialize num_duration_cols by asking the user how many partial duration columns to use
-        prompt_set_num_duration_cols()
+# handle what to do if there is a new run
+display_new_run_prompt(new_run, thorough_refresh_status=thorough_refresh)
+is_helitack_run = prompt_helitack_status(is_helitack_run)
 
 # get instances of Phase classes. Each one has a run() method to run its phase
-p_1 = Phase_1()  # for collecting runs and their inputs
-p_2 = Phase_2()  # for preprocessing - calculating runtime & area, inserting duration cols, etc.
-p_3 = Phase_3()  # for querying performance metrics - totals and at pseudo-random times
-p_4 = Phase_4()  # for finalizing data - cleaning, dropping, adding ratio columns, etc.
+p_1 = Phase_1(debug_mode=True)  # collecting runs and their inputs
+p_2 = Phase_2()  # preprocessing - calc runtime & area, insert duration cols.
+p_3 = Phase_3()  # querying performance metrics - totals and at pseudo-random times
+# finalizing data - cleaning, dropping, adding ratio columns, etc.
+p_4 = Phase_4(helitack_status=is_helitack_run)
 phases = [p_1, p_2, p_3, p_4]
-
 
 # run phases that have not yet been run
 for i, phase in enumerate(phases):
@@ -63,7 +64,12 @@ for i, phase in enumerate(phases):
     phase_number = i+1
     # if phase has previously been finished, move on to next stage
     if is_phase_finished(phase_number):
-        print(colored(f"\n\nPhase {phase_number} has already been completed. Moving on.", "green"))
+        # If all phases have already been completed give a message on setting new_run to True
+        if phase_number == 4:
+            print("entire work flow has already been completed. Perhaps you would like a new run? If so, set new_run to be True.")
+            break
+        print(colored(
+            f"\n\nPhase {phase_number} has already been completed. Moving on.", "green"))
     # for each unfinished phase, run the phase, then set phase_finished of that stage to True
     else:
         # run the phase and see what the resulting success flag is
@@ -71,7 +77,8 @@ for i, phase in enumerate(phases):
         success = phase.run()
         # if phase isn't successful, break
         if not success:
-            print(colored(f"\nProgram stop caused by phase {phase_number}\n", "magenta"))
+            print(
+                colored(f"\nProgram stop caused by phase {phase_number}\n", "magenta"))
             break
         # otherwise, set phase as finished and move on to the next phase
         set_phase_finished(phase_number)
