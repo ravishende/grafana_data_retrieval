466b705802026ab64c5e250f79ff0796e9d1a04b
ravishende <ravi.sameer.shende@gmail.com>
2024-08-02 11:52:34 -0700

    minor updates to formatting of inputs.py

--- inputs.py
+++ inputs.py
@@ -13,6 +13,7 @@ DEFAULT_DURATION = '10m'
 # graphs
 DEFAULT_GRAPH_STEP = '1m'
 DEFAULT_GRAPH_TIME_OFFSET = '1h'
-DEFAULT_FINAL_GRAPH_TIME = datetime.now() # must be a datetime object
+DEFAULT_FINAL_GRAPH_TIME = datetime.now()  # must be a datetime object
 # requerying
-REQUERY_GRAPH_STEP_DIVISOR = 10  # a value of 10 means there will be 10x the number of datapoints for a given timeframe
+# a value of 10 means there will be 10x the number of datapoints for a given timeframe
+REQUERY_GRAPH_STEP_DIVISOR = 10
