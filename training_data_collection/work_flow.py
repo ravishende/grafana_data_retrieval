dbdd30453f72905657c3b5d97f22e49da016d537
ravishende <ravi.sameer.shende@gmail.com>
2024-08-05 11:47:17 -0700

    minor comment chagnes

--- training_data_collection/work_flow.py
+++ training_data_collection/work_flow.py
@@ -33,13 +33,13 @@ Finally, make sure that new_run is set to True at the start of the Main Program 
 ========================
 '''
 
-# if this is the first time running it with new data, set new_run to True. Otherwise, if you are partway through running it, set new_run to False.
+# If this is the first time running it with new data, set new_run to True. Otherwise, if you are partway through running it, set new_run to False.
 new_run = True
-# if running into unexpected issues in phase 1 with paths and run uuids not matching up, try setting thorough_refresh to True. Otherwise, keep it False to not waste time regathering paths.
+# If running into unexpected issues in phase 1 with paths and run uuids not matching up, try setting thorough_refresh to True. Otherwise, keep it False to not waste time regathering paths.
 thorough_refresh = False
-# if we're looking at helitack (nonstandard - dev) runs, set this to True, otherwise by default, set it to False
+# If we're collecting helitack (nonstandard - dev) runs, set this to True, otherwise by default, set it to False
 is_helitack_run = False
-# if the "..._total" columns should be more than just cpu_usage and mem_usage
+# If the "..._total" columns should be more than just cpu_usage and mem_usage, set this to True
 all_totals_included = True
 
 '''
@@ -48,26 +48,26 @@ all_totals_included = True
 ========================
 '''
 include_all_totals_metrics(all_totals_included)
-# handle what to do if there is a new run
+# Handle what to do if there is a new run
 display_new_run_prompt(new_run, thorough_refresh_status=thorough_refresh)
 is_helitack_run = prompt_helitack_status(is_helitack_run)
 
-# get instances of Phase classes. Each one has a run() method to run its phase
-# collecting runs and their inputs
+# Get instances of Phase classes. Each one has a run() method to run its phase
+# Phase 1 - collecting runs and their inputs
 p_1 = Phase_1(debug_mode=True)
-# preprocessing - calc runtime & area, insert duration cols.
+# Phase 2 - preprocessing - calc runtime & area, insert duration cols.
 p_2 = Phase_2()
-# querying performance metrics - totals and at pseudo-random times
+# Phase 3 - querying performance metrics - totals and at pseudo-random times
 p_3 = Phase_3()
-# finalizing data - cleaning, dropping, adding ratio columns, etc.
+# Phase 4 - finalizing data - cleaning, dropping, adding ratio columns, etc.
 p_4 = Phase_4(helitack_status=is_helitack_run)
 phases = [p_1, p_2, p_3, p_4]
 
-# run phases that have not yet been run
+# Run phases that have not yet been run
 for i, phase in enumerate(phases):
-    # get phase number - start at 1, not 0
+    # Get phase number (start at 1, not 0)
     phase_number = i+1
-    # if phase has previously been finished, move on to next stage
+    # If phase has previously been finished, move on to next stage
     if is_phase_finished(phase_number):
         # If all phases have already been completed give a message on setting new_run to True
         if phase_number == 4:
@@ -75,16 +75,16 @@ for i, phase in enumerate(phases):
             break
         print(colored(
             f"\n\nPhase {phase_number} has already been completed. Moving on.", "green"))
-    # for each unfinished phase, run the phase, then set phase_finished of that stage to True
+    # For each unfinished phase, run the phase, then set phase_finished of that stage to True
     else:
-        # run the phase and see what the resulting success flag is
+        # Run the phase and see what the resulting success flag is
         print(colored(f"\n\nBeginning Phase {phase_number}...", "magenta"))
         success = phase.run()
-        # if phase isn't successful, break
+        # If phase isn't successful, break
         if not success:
             print(
                 colored(f"\nProgram stop caused by phase {phase_number}\n", "magenta"))
             break
-        # otherwise, set phase as finished and move on to the next phase
+        # Otherwise, set phase as finished and move on to the next phase
         set_phase_finished(phase_number)
         print(colored(f"\n\nPhase {phase_number} complete!\n", "green"))
