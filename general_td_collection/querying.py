0a99e2e04b0904d9b5faf1d99e42af1a2ba009ca
ravishende <ravi.sameer.shende@gmail.com>
2024-08-09 16:25:45 -0700

    continued flushing out querying functionality in querying.py

--- general_td_collection/querying.py
+++ general_td_collection/querying.py
@@ -6,11 +6,12 @@ import warnings
 import sys
 import os
 # Adjust the path to go up one level
-sys.path.append("../../grafana_data_retrieval")
+sys.path.append("../grafana_data_retrieval")
 current = os.path.dirname(os.path.realpath(__file__))
 parent = os.path.dirname(current)
 sys.path.append(parent)
 from helpers.time_functions import calculate_offset, delta_to_time_str, datetime_ify
+from graphs import Graphs
 # autopep8: on
 
 
@@ -31,7 +32,8 @@ class Query_handler():
         self.node_regex = node_regex
         self.pod_regex = pod_regex
         self.namespace_regex = namespace_regex
-        self.filter_str = self.init_filter_str()
+        self.filter_str = self._init_filter_str()
+
         self.duration = duration
         # csv files
         self._read_file = read_file
@@ -48,7 +50,8 @@ class Query_handler():
             raise ValueError(
                 "at most one of component_name or component_regex should be defined.")
         # give a warning if it doesn't seem like the filter str is being used correctly
-        known_k8s_components = ['node', 'pod', 'namespace', 'cluster']
+        known_k8s_components = ['node', 'pod', 'namespace', 'cluster', 'job',
+                                'container', 'Hostname', 'UUID', 'device', 'endpoint', 'service']
         if component not in known_k8s_components:
             warnings.warn(
                 f"Unknown component '{component}'. Known components are: {known_k8s_components}")
@@ -61,7 +64,7 @@ class Query_handler():
         return ""
 
     # get filter str using passed in values during init
-    def init_filter_str(self) -> str:
+    def _init_filter_str(self) -> str:
         # get individual filters
         node_filter = self._get_component_filter_str(
             "node", self.node, self.node_regex)
@@ -88,54 +91,38 @@ class Query_handler():
         # graph queries
         graph_queries_no_sum_by = {
             # total gpu usage = gpu_utilization% by pod but averaging out all the pods
-            'total_gpu_usage': 'avg_over_time(namespace_gpu_utilization{' + self.filter_str + '}[' + duration + '] offset ' + offset + ')',
-            'requested_gpus': 'count(DCGM_FI_DEV_GPU_TEMP{' + self.filter_str + '} offset ' + offset + ')'
-        }
-        # table queries
-        table_queries_no_sum_by = {
-            'gpu_utilization': 'sum(DCGM_FI_DEV_GPU_UTIL{' + self.filter_str + '}[' + duration + '] offset ' + offset + ')/count(DCGM_FI_DEV_GPU_UTIL{' + self.filter_str + '}[' + duration + '] offset ' + offset + ')',
-
-            'gpus_requested': 'count(DCGM_FI_DEV_GPU_TEMP{' + self.filter_str + '}[' + duration + '] offset ' + offset + ')'
+            'total_gpu_usage': 'avg_over_time(namespace_gpu_utilization{' + self.filter_str + '}',
+            'requested_gpus': 'count(DCGM_FI_DEV_GPU_TEMP{' + self.filter_str + '})'
         }
         queries = {}
-        sum_by = None
-        return queries, sum_by
+        return queries
 
-    def get_gpu_compute_resource_queries(self, start, duration_seconds: int | float) -> tuple[dict, list]:
-        start = datetime_ify(start)
-        duration_seconds = int(duration_seconds)
-        offset = calculate_offset(start, duration_seconds)
-        duration = delta_to_time_str(timedelta(seconds=duration_seconds))
+    def get_gpu_compute_resource_queries(self) -> dict[str, str]:
         # graph queries
         queries = {
-            'gpu_utilization': 'DCGM_FI_DEV_GPU_UTIL * on (namespace, pod) group_left(node) node_namespace_pod:kube_pod_info:{' + self.filter_str + '}[' + duration + '] offset ' + offset,
-            'memory_copy_utilization': 'DCGM_FI_DEV_MEM_COPY_UTIL * on (namespace, pod) group_left(node) node_namespace_pod:kube_pod_info:{' + self.filter_str + '}[' + duration + '] offset ' + offset,
-            'power': 'DCGM_FI_DEV_POWER_USAGE * on (namespace, pod) group_left(node) node_namespace_pod:kube_pod_info:{' + self.filter_str + '}[' + duration + '] offset ' + offset,
-            'temperature': 'DCGM_FI_DEV_GPU_TEMP * on (namespace, pod) group_left(node) node_namespace_pod:kube_pod_info:{' + self.filter_str + '}[' + duration + '] offset ' + offset,
-            'fan_speed': 'ipmi_fan_speed * on (namespace, pod) group_left(node) node_namespace_pod:kube_pod_info:{' + self.filter_str + '}[' + duration + '] offset ' + offset
+            'gpu_utilization': 'DCGM_FI_DEV_GPU_UTIL * on (namespace, pod) group_left(node) node_namespace_pod:kube_pod_info:{' + self.filter_str + '}',
+            'memory_copy_utilization': 'DCGM_FI_DEV_MEM_COPY_UTIL * on (namespace, pod) group_left(node) node_namespace_pod:kube_pod_info:{' + self.filter_str + '}',
+            'power': 'DCGM_FI_DEV_POWER_USAGE * on (namespace, pod) group_left(node) node_namespace_pod:kube_pod_info:{' + self.filter_str + '}',
+            'temperature': 'DCGM_FI_DEV_GPU_TEMP * on (namespace, pod) group_left(node) node_namespace_pod:kube_pod_info:{' + self.filter_str + '}',
+            'fan_speed': 'ipmi_fan_speed * on (namespace, pod) group_left(node) node_namespace_pod:kube_pod_info:{' + self.filter_str + '}'
         }
-        sum_by = ["namespace", "pod"]
-        return queries, sum_by
+        return queries
 
-    def get_rgw_queries(self, start, duration_seconds: int | float) -> tuple[dict, list]:
-        start = datetime_ify(start)
-        duration_seconds = int(duration_seconds)
-        offset = calculate_offset(start, duration_seconds)
-        duration = delta_to_time_str(timedelta(seconds=duration_seconds))
+    def get_rgw_queries(self) -> dict[str, str]:
         # graph queries
         queries = {
-            'queue_length': 'sum by(instance) (ceph_rgw_qlen{' + self.filter_str + '}[' + duration + '] offset ' + offset + ')',
-            'rgw_cache_hit': 'sum by(instance) (irate(ceph_rgw_cache_hit{' + self.filter_str + '}[' + duration + '] offset ' + offset + '))',
-            'rgw_cache_miss': 'sum by(instance) (irate(ceph_rgw_cache_miss{' + self.filter_str + '}[' + duration + '] offset ' + offset + '))',
-            'rgw_gets': 'sum by(instance) (irate(ceph_rgw_get{' + self.filter_str + '}[' + duration + '] offset ' + offset + '))',
-            'rgw_puts': 'sum by(instance) (irate(ceph_rgw_put{' + self.filter_str + '}[' + duration + '] offset ' + offset + '))',
-            'rgw_failed_req': 'sum by(instance) (irate(ceph_rgw_failed_req{' + self.filter_str + '}[' + duration + '] offset ' + offset + '))'
+            'rgw_queue_length': 'sum by(instance) (ceph_rgw_qlen{' + self.filter_str + '}',
+            'rgw_cache_hit': 'sum by(instance) (irate(ceph_rgw_cache_hit{' + self.filter_str + '}))',
+            'rgw_cache_miss': 'sum by(instance) (irate(ceph_rgw_cache_miss{' + self.filter_str + '}))',
+            'rgw_gets': 'sum by(instance) (irate(ceph_rgw_get{' + self.filter_str + '}))',
+            'rgw_puts': 'sum by(instance) (irate(ceph_rgw_put{' + self.filter_str + '}))',
+            'rgw_failed_req': 'sum by(instance) (irate(ceph_rgw_failed_req{' + self.filter_str + '}))'
         }
-        sum_by = ["instance"]
-        return queries, sum_by
+
+        return queries
 
     # queries from ../training_data_handling/work_flow.py
-    def get_cpu_compute_resource_queries(self, start, duration_seconds: int | float) -> tuple[dict, list]:
+    def get_cpu_compute_resource_queries(self, start, duration_seconds: int | float) -> dict[str, str]:
         start = datetime_ify(start)
         duration_seconds = int(duration_seconds)
         offset = calculate_offset(start, duration_seconds)
@@ -163,9 +150,40 @@ class Query_handler():
             'received_bandwidth': 'sum by (pod) (increase(container_network_receive_bytes_total{' + self.filter_str + '}[' + duration + '] offset ' + offset + '))'
         }
 
-        sum_by = ['pod']
-        # assemble the final query
-        return queries, sum_by
+        return queries
+
+    def _get_queries(self, gpu_queries=False, gpu_compute_resource_queries=False, rgw_queries=False, cpu_compute_resource_queries=False):
+        graph_queries = {}
+        non_graph_queries = {}
+
+        # TODO: figure out how to deal with sum_by
+        # idea: sum_by never matters because we always want one datapoint for each cell
+        # therefore, we should just sum everything, keeping sum_by as None.
+        if gpu_queries:
+            new_queries = self.get_gpu_queries()
+            graph_queries.update(new_queries['graph'])
+        if gpu_compute_resource_queries:
+            new_queries = self.get_gpu_compute_resource_queries()
+            graph_queries.update(new_queries['graph'])
+        if cpu_compute_resource_queries:
+            new_queries = self.get_cpu_compute_resource_queries()
+            graph_queries.update(new_queries['graph'])
+        if rgw_queries:
+            new_queries = self.get_rgw_queries()
+            graph_queries.update(new_queries['graph'])
+
+        return {'graph': graph_queries, 'non_graph': non_graph_queries}
+
+    # given a row of a dataframe, a graphs class instantiation, and a dict of queries for graphs,
+    # return a queried version of that row,
+    # with the new columns being the graph titles prepended with 'graph_'
+    def _query_row_for_graphs(self, row: pd.Series, graphs_class: Graphs, graph_queries: dict[str, str]) -> pd.Series:
+        # sum by none since we're eventually getting it all down to one data point --> no need to split it further
+        graphs_dict = graphs_class.get_graphs_from_queries(
+            queries_dict=graph_queries, sum_by=None, start=row['start'], end=row['stop'])
+        for title, data in graphs_dict.items():
+            row[title] = data
+        return row
 
     def query_df(self, df: pd.DataFrame = None, batch_size: int = 5, gpu_queries=False, gpu_compute_resource_queries=False, rgw_queries=False, cpu_compute_resource_queries=False) -> pd.DataFrame:
         # handle user input
@@ -178,8 +196,14 @@ class Query_handler():
                 raise ValueError(
                     f"no df passed in, and default read file ({self._read_file}) cannot be read.")
 
-        # query in batches
+        queries_by_type = self._get_queries()
+        # TODO: query in batches
         print(f"Querying in batches of {batch_size} rows...")
-        # TODO: add logic for querying
-        #   TODO: logic of graph querying vs normal querying
+        # query graphs
+        graphs_class = Graphs()
+        graph_queries = queries_by_type['graph']
+        df = df.apply(self._query_row_for_graphs,
+                      args=(graphs_class, graph_queries))
+        df.to_csv("csvs/graphs_queried.csv")
+        # TODO: query normal rows (maybe with table.py?)
         return df
